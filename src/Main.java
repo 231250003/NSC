22898c07a407c7b9d85de1d6aaa8b0be1897dce0
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-02-25 18:54:07 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:54:07 up 22 min,  2 users,  load average: 0.09, 0.21, 0.17 c40eb1d4323636683afe003ad5fd8c2c2558c4eb

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,17 @@ import java.nio.charset.StandardCharsets;
 public class Main {
 
     public static void main(String[] args) {
-        String filePath = args[0];
         if (args.length != 1) {
             System.out.println("crz");
             System.err.println("Usage: java Main <filename>");
             System.exit(1);
         }
+        String filePath = args[0];
         try {
             // 读取整个文件并按行输出
             Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)
                     .forEach(System.out::println);
         } catch (IOException e) {
-            System.out.println("crz");
             throw new RuntimeException(e);
         }
     }
