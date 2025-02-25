00a59df12a8a9db99f84db41cdc6bdbcba64b254
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-02-25 17:47:22 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:47:22 up 41 min,  1 user,  load average: 0.13, 1.36, 5.65 fc5c2890fc21e6091e092d2e4dde353c564b90fb

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
         String filePath = args[0];
+        if (args.length != 1) {
+            System.err.println("Usage: java Main <filename>");
+            System.exit(1);
+        }
         try {
             // 读取整个文件并按行输出
             Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)
