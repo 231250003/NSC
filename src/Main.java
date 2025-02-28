cefa23de2da7e63c6f51570fc25d8a54c7d41472
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-02-28 16:23:50 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:23:50 up 9 min,  1 user,  load average: 0.12, 0.30, 0.22 aa6246c3a4bbf8d2787dff1757aa59ff9fd747ea

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,18 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.charset.StandardCharsets;
 public class Main {
 
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Usage: java Main <filename>");
-            System.exit(1);
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.err.println("input path is required");
         }
-        String filePath = args[0];
-        try {
-            // 读取整个文件并按行输出
-            Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)
-                    .forEach(System.out::println);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        String source = args[0];
+        CharStream input = CharStreams.fromFileName(source);
+        SysYLexer sysYLexer = new SysYLexer(input);
     }
 }
