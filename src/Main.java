667cff43c7583eeae22c41c38e63d8db6d30d619
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-03-12 00:04:03 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:04:03 up  5:41,  1 user,  load average: 0.06, 0.12, 0.15 74985117fec7b871738cbb5e2368759a41eb6f0

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ public class Main {
         parser.removeErrorListeners();
         MyErrorListener parser_errorListener=new MyErrorListener(2);
         parser.addErrorListener(parser_errorListener);
-
+        parser.setErrorHandler(new DefaultErrorStrategy());
         parser.compUnit();
 
 //        if(errorListener.hasErrorInformation()){
