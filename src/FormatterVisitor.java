4770b2fa0fcce9e525ae2e6e7899ce1f5b13d9e1
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-03-11 20:23:59 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:23:59 up  2:14,  1 user,  load average: 0.40, 0.24, 0.19 206e369aeccb9bd567d722ff86a38269bba5a0bd

--- src/FormatterVisitor.java
+++ src/FormatterVisitor.java
@@ -3,7 +3,7 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.RuleNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
-public class FormatterVisitor extends SysYParserVisitor<Void> {
+public class FormatterVisitor implements SysYParserVisitor<Void> {
     public Void visitProgram(SysYParser.ProgramContext ctx) {
         visitChildren(ctx); // 访问所有子节点
         return null;
@@ -17,10 +17,57 @@ public class FormatterVisitor extends SysYParserVisitor<Void> {
         visitChildren(ctx);
         return null;
     }
+
+    @Override
+    public Void visitFuncType(SysYParser.FuncTypeContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitFuncFParams(SysYParser.FuncFParamsContext ctx) {
+        return null;
+    }
+
     public Void visitDecl(SysYParser.DeclContext ctx) {
         visitChildren(ctx); // 访问声明内容
         return null;
     }
+
+    @Override
+    public Void visitConstDecl(SysYParser.ConstDeclContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitBType(SysYParser.BTypeContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitConstDef(SysYParser.ConstDefContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitConstInitVal(SysYParser.ConstInitValContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitVarDecl(SysYParser.VarDeclContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitVarDef(SysYParser.VarDefContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitInitVal(SysYParser.InitValContext ctx) {
+        return null;
+    }
+
     public Void visitStmt(SysYParser.StmtContext ctx) {
         visitChildren(ctx);
         return null;
@@ -32,6 +79,52 @@ public class FormatterVisitor extends SysYParserVisitor<Void> {
         visitChildren(ctx);
         return null;
     }
+
+    @Override
+    public Void visitCond(SysYParser.CondContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitLVal(SysYParser.LValContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitNumber(SysYParser.NumberContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitUnaryOp(SysYParser.UnaryOpContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitFuncRParams(SysYParser.FuncRParamsContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitParam(SysYParser.ParamContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitConstExp(SysYParser.ConstExpContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visit(ParseTree parseTree) {
+        return null;
+    }
+
+    @Override
+    public Void visitChildren(RuleNode ruleNode) {
+        return null;
+    }
+
     public Void visitTerminal(TerminalNode node) {
         String text = node.getText();
         if (text.equals("+") || text.equals("-") || text.equals("*")) {
@@ -42,6 +135,12 @@ public class FormatterVisitor extends SysYParserVisitor<Void> {
 
         return null;
     }
+
+    @Override
+    public Void visitErrorNode(ErrorNode errorNode) {
+        return null;
+    }
+
     @Override
     public Void visitFuncFParam(SysYParser.FuncFParamContext ctx) {
         visitChildren(ctx);
@@ -52,6 +151,11 @@ public class FormatterVisitor extends SysYParserVisitor<Void> {
         System.out.println("{");
         visitChildren(ctx); // 访问block中的内容
         System.out.println("}");
+        return null;
+    }
+
+    @Override
+    public Void visitBlockItem(SysYParser.BlockItemContext ctx) {
         return null;
     }
 }
