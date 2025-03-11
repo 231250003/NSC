80ba2af37aaee8dafe3566d5473926e5e163dc9c
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-03-11 19:56:55 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:56:55 up  1:44,  1 user,  load average: 0.18, 0.16, 0.16 b9f808a1395b4c955bb640c340ed3964975011

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ public class Main {
         MyErrorListener parser_errorListener=new MyErrorListener(2);
         parser.addErrorListener(parser_errorListener);
 
-        parser.compUnit();
+       // parser.compUnit();
 
         if(errorListener.hasErrorInformation()){
             errorListener.printLexerErrorInformation(false);
