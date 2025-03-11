96ea227cec1e508db177b1b066b3ce2cf1163fe3
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-03-12 00:21:07 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:21:07 up  5:59,  1 user,  load average: 0.09, 0.13, 0.11 a2e47be4ceefbd978c466ec2168fdf4268fb6fb

--- src/Main.java
+++ src/Main.java
@@ -28,24 +28,26 @@ public class Main {
         MyErrorListener parser_errorListener=new MyErrorListener(2);
         parser.addErrorListener(parser_errorListener);
         parser.setErrorHandler(new DefaultErrorStrategy() {
-                                   @Override
-                                   public void recover(Parser recognizer, RecognitionException e) {
-                                       Token token = recognizer.getCurrentToken();
-                                       String msg = String.format("Error type B at Line %d: unexpected token '%s'",
-                                               token.getLine(), token.getText());
-                                       System.out.println(msg);
-                                   }
+            @Override
+            public void recover(Parser recognizer, RecognitionException e) {
+                Token token = recognizer.getCurrentToken();
+                System.out.printf("Error type B at Line %d: unexpected token '%s'%n",
+                        token.getLine(), token.getText());
 
-                                   @Override
-                                   public Token recoverInline(Parser recognizer) throws RecognitionException {
-                                       Token token = recognizer.getCurrentToken();
-                                       throw new InputMismatchException(recognizer);
-                                   }
+                // **手动跳过错误的 Token**
+                recognizer.consume();
+            }
 
-                                   @Override
-                                   public void sync(Parser recognizer) throws RecognitionException {
-                                       // 取消 ANTLR 默认的同步机制，防止跳过 Token
-                                   }
+            @Override
+            public Token recoverInline(Parser recognizer) throws RecognitionException {
+                Token token = recognizer.getCurrentToken();
+                throw new InputMismatchException(recognizer);
+            }
+
+            @Override
+            public void sync(Parser recognizer) throws RecognitionException {
+                // 取消默认的错误恢复同步机制，防止跳过后续 Token
+            }
                                });
         parser.compUnit();
 
