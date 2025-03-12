311588583f818971151aa0c1e66162a7099363a6
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-03-12 13:01:48 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:01:48 up  5:11,  1 user,  load average: 0.31, 0.40, 0.32 162cfc15318c19c76557409147b596d591d0c27a

--- src/FormatterVisitor.java
+++ src/FormatterVisitor.java
@@ -425,9 +425,9 @@ public class FormatterVisitor extends SysYParserBaseVisitor<Void> {
             System.out.println();
         }
         else if(parent instanceof SysYParser.StmtContext) {
+            System.out.print("112312");
             SysYParser.StmtContext stmtCtx = (SysYParser.StmtContext) parent;
-            if (stmtCtx.IF() != null || stmtCtx.WHILE() != null ) {
-                System.out.print("112312");
+            if (stmtCtx.IF() != null || stmtCtx.WHILE() != null) {
                 System.out.print(" {");
                 System.out.println();
             }
