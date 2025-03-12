10cea0176709a41f5f4953c62a629ce638721053
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-03-12 13:02:28 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:02:28 up  5:12,  1 user,  load average: 0.36, 0.40, 0.32 c8726007bcf93878231e9b8c2f35a961d199be36

--- src/FormatterVisitor.java
+++ src/FormatterVisitor.java
@@ -421,6 +421,7 @@ public class FormatterVisitor extends SysYParserBaseVisitor<Void> {
     public Void visitBlock(SysYParser.BlockContext ctx) {
         ParserRuleContext parent = ctx.getParent();
         if( parent instanceof SysYParser.FuncDefContext){
+            System.out.print("13124234");
             System.out.print(" {");
             System.out.println();
         }
