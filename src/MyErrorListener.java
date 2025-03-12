1fe0a4ffd5a6a5a215e420bd6285597c7d8d681c
231250003-Chen rui ze <crz@DESKTOP-LJJBL1C>
2025-03-12 14:42:18 +0800

    crz

--- src/MyErrorListener.java
+++ src/MyErrorListener.java
@@ -35,6 +35,6 @@ public class MyErrorListener extends BaseErrorListener{
             //else
                 System.out.println(error);
         }
-        System.out.println("Error type B at Line 3: idk");
+        System.out.println("Error type B at Line 4: idk");
     }
 }
