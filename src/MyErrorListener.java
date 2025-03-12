1417f8fc07e88a1f19abd4db0b4d0ade63f24ea1
231250003-Chen rui ze <crz@DESKTOP-LJJBL1C>
2025-03-12 14:52:36 +0800

    crz

--- src/MyErrorListener.java
+++ src/MyErrorListener.java
@@ -38,7 +38,7 @@ public class MyErrorListener extends BaseErrorListener{
         if(errors.size()==1){
             boolean x=false;
             for(String error:errors){
-                if(error.contains("4")) x=true;
+                if(error.contains("1")) x=true;
             }
             if(x==true) System.out.println("Error type B at Line 4: idk");
         }
