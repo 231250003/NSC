14677757a0412edadaa664db8c3f828e1ca7d6db
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-03-12 13:04:35 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:04:35 up  5:14,  1 user,  load average: 0.14, 0.29, 0.29 6156d5bae6393fc1ad6d31b3f134f76b71517e1f

--- src/FormatterVisitor.java
+++ src/FormatterVisitor.java
@@ -427,12 +427,12 @@ public class FormatterVisitor extends SysYParserBaseVisitor<Void> {
         else if(parent instanceof SysYParser.StmtContext) {
             SysYParser.StmtContext stmtCtx = (SysYParser.StmtContext) parent;
             if (stmtCtx.IF() != null || stmtCtx.WHILE() != null) {
+                System.out.print("crzzzzz");
                 System.out.print(" {");
                 System.out.println();
             }
             else {
                 System.out.print("crz");
-
                 printIndent();
                 System.out.print("{");
                 System.out.println();
