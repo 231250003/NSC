ee6bde9fdc855a085f2053122b0537b0b7d69136
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-03-12 10:48:05 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:48:05 up  3:14,  1 user,  load average: 0.90, 0.94, 0.68 3d9482a153fb921f1984a2e3b5cbc98ca26d2f

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {
         parser.removeErrorListeners();
         MyErrorListener parser_errorListener=new MyErrorListener(2);
         parser.addErrorListener(parser_errorListener);
-       parser.compUnit();
+       parser.program();
 
 //        if(errorListener.hasErrorInformation()){
 //            errorListener.printLexerErrorInformation(false);
@@ -38,6 +38,7 @@ public class Main {
         }
         else
         {
+            parser.reset();
             ParseTree tree = parser.program();
             FormatterVisitor visitor = new FormatterVisitor();
             visitor.visit(tree);
