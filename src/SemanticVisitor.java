d5f7c04e74e6ede001bdab54e6de592c7ff367c1
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-03-21 13:44:22 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:44:22 up  5:45,  1 user,  load average: 0.91, 0.64, 0.56 c04921764b44e31fa14422af71c94dd7d4ed7be3

--- src/SemanticVisitor.java
+++ src/SemanticVisitor.java
@@ -46,10 +46,10 @@ public class SemanticVisitor extends SysYParserBaseVisitor<Void> {
         }
         List<Symbol> params=null;
         if(ctx.funcFParams()!=null){
+            System.out.println("crzzz");
             params=getFuncFParams(ctx.funcFParams());
         }
         FunctionType functionType = new FunctionType(retType, params);
-        System.out.println(retType.toString());
         symbolTable.addGlobal(new Symbol(funcName,functionType));
         visit_block(ctx.block(),params);
         return null;
