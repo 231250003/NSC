a0a9548e293e195c963a014473175e240f520513
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-03-21 14:53:46 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:53:46 up  7:01,  1 user,  load average: 0.75, 0.57, 0.45 c3f7f514e6aa3987f1fca57f2cc4e2ad70d2d4cd

--- src/SemanticVisitor.java
+++ src/SemanticVisitor.java
@@ -225,6 +225,8 @@ public class SemanticVisitor extends SysYParserBaseVisitor<Void> {
                 }
             }
         }
+        System.out.println(symbol.name);
+        System.out.println(symbol.type);
         return symbol;
     }
 
@@ -390,7 +392,7 @@ public class SemanticVisitor extends SysYParserBaseVisitor<Void> {
             return null;
         }
         if(ctx.exp()!=null) {
-            System.out.println(s.type);
+           // System.out.println(s.type);
             if(s.type instanceof IntType){
 //                System.out.println(s.name);
 //                System.out.println(s.type);
