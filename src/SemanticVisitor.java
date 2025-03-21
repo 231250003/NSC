bd54dfd0d4cbcd2e850bdc94a708299295000bcd
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-03-21 14:31:16 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:31:16 up  6:36,  1 user,  load average: 1.43, 0.68, 0.45 f9d531ded05680bdb9b0b87993dec36b42753f

--- src/SemanticVisitor.java
+++ src/SemanticVisitor.java
@@ -381,7 +381,6 @@ public class SemanticVisitor extends SysYParserBaseVisitor<Void> {
         String name=ctx.IDENT().getText();
         Symbol s=symbolTable.get_name_matched_symbol(name);
         if(s==null){
-            System.out.println("crzzz");
             OutputHelper.printSemanticError(ErrorType.UNDECLARED_VARIABLE,ctx.getStart().getLine());
             return null;
         }
@@ -398,6 +397,7 @@ public class SemanticVisitor extends SysYParserBaseVisitor<Void> {
                 int dim=ctx.L_BRACKT().size();
                 int org_dim=((ArrayType)s.type).dim;
                 if(dim>org_dim) {
+                    System.out.println("crzzz");
                     OutputHelper.printSemanticError(ErrorType.UNDECLARED_VARIABLE,ctx.getStart().getLine());
                     return null;
                 }
