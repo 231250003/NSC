8fb3a79830d345ca1c0530c1c2387a4a90fe0448
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-03-21 14:56:58 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:56:58 up  7:05,  1 user,  load average: 0.59, 0.57, 0.47 69865ad73b9c82c95f40479b62a61e34a644430

--- src/SemanticVisitor.java
+++ src/SemanticVisitor.java
@@ -199,6 +199,7 @@ public class SemanticVisitor extends SysYParserBaseVisitor<Void> {
         Symbol symbol=new Symbol();
         symbol.name=ctx.IDENT().getText();
         if(ctx.initVal()==null) {
+            System.out.println("crz");
             if (ctx.constExp() != null) {
                 symbol.type = new ArrayType();
                 ((ArrayType) symbol.type).dim = ctx.constExp().size();
