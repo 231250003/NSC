2cd2838a4edf043030b961e15e1b4e336f16c48a
231250003-Chen rui ze <crz@DESKTOP-LJJBL1C>
2025-03-22 02:19:30 +0800

    crz

--- src/semantic_check/ErrorType.java
+++ src/semantic_check/ErrorType.java
@@ -8,7 +8,7 @@ public enum ErrorType {
     TYPE_MISMATCH_ASSIGNMENT(5, "赋值号两侧类型不匹配"),
     TYPE_MISMATCH_OPERATOR(6, "运算符需求类型与提供类型不匹配"),
     TYPE_MISMATCH_RETURN(7, "返回值类型不匹配"),
-    //FUNCTION_ARGUMENT_MISMATCH(8, "函数参数不适用"),
+    FUNCTION_ARGUMENT_MISMATCH(0, "函数参数不适用"),
     INDEXING_NON_ARRAY(9, "对非数组使用下标运算符"),
     CALLING_NON_FUNCTION(10, "对变量使用函数调用"),
     INVALID_ASSIGNMENT_TARGET(11, "赋值号左侧非变量或数组元素");
