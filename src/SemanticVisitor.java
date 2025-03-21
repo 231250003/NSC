679e014c5ede41026a10628a861910108cf2b50d
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-03-21 14:37:21 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:37:21 up  6:43,  1 user,  load average: 1.23, 0.59, 0.45 2d55af325ee85371a8082e561c78474ec0f69c9

--- src/SemanticVisitor.java
+++ src/SemanticVisitor.java
@@ -238,6 +238,8 @@ public class SemanticVisitor extends SysYParserBaseVisitor<Void> {
         if (ctx.lVal() != null && ctx.exp() != null) {
            Type left=getlVal(ctx.lVal());//lval guarantees that it is an var or array like a,a[],a[][]
            Type right=getExp(ctx.exp());
+           System.out.println(left);
+           System.out.println(right);
            if(left!=null && right!=null){
                 if(!left.equals(right)){
                     OutputHelper.printSemanticError(ErrorType.TYPE_MISMATCH_ASSIGNMENT,ctx.getStart().getLine());
@@ -385,7 +387,6 @@ public class SemanticVisitor extends SysYParserBaseVisitor<Void> {
             return null;
         }
         if(ctx.exp()!=null) {
-            System.out.println(s.type);
             if(s.type instanceof IntType){
                 OutputHelper.printSemanticError(ErrorType.INDEXING_NON_ARRAY,ctx.getStart().getLine());
                 return null;
