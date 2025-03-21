dd2a7212efbc3eb2964b52a9602c3c54474a7672
231250003-Chen rui ze <crz@DESKTOP-LJJBL1C>
2025-03-22 02:20:58 +0800

    crz

--- src/semantic_check/ErrorType.java
+++ src/semantic_check/ErrorType.java
@@ -6,9 +6,9 @@ public enum ErrorType {
     REPEATED_VARIABLE_DECLARATION(3, "变量重复声明"),
     REPEATED_FUNCTION_DEFINITION(4, "函数重复定义"),
     TYPE_MISMATCH_ASSIGNMENT(5, "赋值号两侧类型不匹配"),
-    TYPE_MISMATCH_OPERATOR(6, "运算符需求类型与提供类型不匹配"),
+    TYPE_MISMATCH_OPERATOR(0, "运算符需求类型与提供类型不匹配"),
     TYPE_MISMATCH_RETURN(7, "返回值类型不匹配"),
-    FUNCTION_ARGUMENT_MISMATCH(0, "函数参数不适用"),
+    FUNCTION_ARGUMENT_MISMATCH(8, "函数参数不适用"),
     INDEXING_NON_ARRAY(9, "对非数组使用下标运算符"),
     CALLING_NON_FUNCTION(10, "对变量使用函数调用"),
     INVALID_ASSIGNMENT_TARGET(11, "赋值号左侧非变量或数组元素");
