33fa9c0075ee8def79e847516632109ad668fd24
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-03 21:08:35 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:08:35 up  8:20,  1 user,  load average: 0.83, 0.51, 0.33 aefe52b7c253a04c3df456fea7acc885a90cdcb1

--- src/IRGenerationVisitor.java
+++ src/IRGenerationVisitor.java
@@ -299,6 +299,8 @@ public class IRGenerationVisitor extends   SysYParserBaseVisitor<LLVMValueRef> {
             // 处理 <, >, <=, >=
             LLVMValueRef left = visit(ctx.cond(0));
             LLVMValueRef right = visit(ctx.cond(1));
+            left = LLVMBuildLoad(builder, left, "load_a");
+            right = LLVMBuildLoad(builder, right, "load_const");
             int predicate;
             if (ctx.LT() != null) {
                 predicate = LLVMIntSLT;
@@ -315,17 +317,22 @@ public class IRGenerationVisitor extends   SysYParserBaseVisitor<LLVMValueRef> {
             LLVMValueRef left = visit(ctx.cond(0));
             LLVMValueRef right = visit(ctx.cond(1));
             int predicate = (ctx.EQ() != null) ? LLVMIntEQ : LLVMIntNE;
-            System.out.println("Left value: " + LLVMPrintValueToString(right).getString());
+            left = LLVMBuildLoad(builder, left, "load_a");
+            right = LLVMBuildLoad(builder, right, "load_const");
             return LLVMBuildICmp(builder, predicate, left, right, "eqcmp");
         }
         else if (ctx.AND() != null) {
             LLVMValueRef left = visit(ctx.cond(0));
             LLVMValueRef right = visit(ctx.cond(1));
+            left = LLVMBuildLoad(builder, left, "load_a");
+            right = LLVMBuildLoad(builder, right, "load_const");
             return LLVMBuildAnd(builder, left, right, "and");
         }
         else if (ctx.OR() != null) {
             LLVMValueRef left = visit(ctx.cond(0));
             LLVMValueRef right = visit(ctx.cond(1));
+            left = LLVMBuildLoad(builder, left, "load_a");
+            right = LLVMBuildLoad(builder, right, "load_const");
             return LLVMBuildOr(builder, left, right, "or");
         }
         else return null;
