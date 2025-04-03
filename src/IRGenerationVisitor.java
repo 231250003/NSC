6accce9f5eb437d892ede8dbd31050f154e36171
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-03 20:49:53 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:49:53 up  8:01,  1 user,  load average: 0.08, 0.14, 0.15 59c476c7d1a0bc716e99fc0435d00823437b321

--- src/IRGenerationVisitor.java
+++ src/IRGenerationVisitor.java
@@ -360,7 +360,7 @@ public class IRGenerationVisitor extends   SysYParserBaseVisitor<LLVMValueRef> {
             if (ctx.ELSE() != null) {
                 elseBlock = LLVMAppendBasicBlock(function, "if.else");
             }
-            System.err.println(firstCond);
+            System.err.println( LLVMBuildZExt(builder, firstCond, LLVMInt32Type(), "cmp_res"););
             System.err.println("dfsfds");
             LLVMBuildCondBr(builder, firstCond, thenBlock, elseBlock == null ? mergeBlock : elseBlock);
             LLVMPositionBuilderAtEnd(builder, thenBlock);
