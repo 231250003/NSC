e3b714cd18f375b1b0c5e95dc2c1fa6e71fa1e9a
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-03 21:01:59 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:01:59 up  8:13,  1 user,  load average: 1.18, 0.41, 0.25 8cd17cff17a1613a1ec0d002a0f1506683a503a9

--- src/IRGenerationVisitor.java
+++ src/IRGenerationVisitor.java
@@ -315,7 +315,8 @@ public class IRGenerationVisitor extends   SysYParserBaseVisitor<LLVMValueRef> {
             LLVMValueRef left = visit(ctx.cond(0));
             LLVMValueRef right = visit(ctx.cond(1));
             int predicate = (ctx.EQ() != null) ? LLVMIntEQ : LLVMIntNE;
-            assert(left!=right);
+            assert (left!=null);
+            assert (right!=null);
             return LLVMBuildICmp(builder, predicate, left, right, "eqcmp");
         }
         else if (ctx.AND() != null) {
