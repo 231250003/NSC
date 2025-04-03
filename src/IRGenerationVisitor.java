8854061da31a2257099a7c010be166f13f60237d
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-03 19:24:57 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:24:57 up  6:36,  1 user,  load average: 0.08, 0.09, 0.12 eb02bfdb3299602bb2d908a8e2a7df2949c162a3

--- src/IRGenerationVisitor.java
+++ src/IRGenerationVisitor.java
@@ -136,10 +136,6 @@ public class IRGenerationVisitor extends   SysYParserBaseVisitor<LLVMValueRef> {
     @Override
     public LLVMValueRef visitExp(SysYParser.ExpContext ctx) {
         if (ctx.number() != null) return visit(ctx.number());
-        else if(ctx.L_PAREN()!=null&&ctx.exp()!=null){
-            System.err.println("121231321");
-            return visit(ctx.exp(0));
-        }
         else if (ctx.IDENT() != null && ctx.L_PAREN() != null) {
             System.err.println("crzasa");
             String funcName = ctx.IDENT().getText();
@@ -174,6 +170,9 @@ public class IRGenerationVisitor extends   SysYParserBaseVisitor<LLVMValueRef> {
                  case "%" :return LLVMBuildSRem(builder, left, right, "mod");
                  default :return null;
             }
+        }
+        else if(ctx.L_PAREN()!=null&&ctx.exp()!=null){
+            return visit(ctx.exp(0));
         }
         else if(ctx.lVal()!=null){
             return visit(ctx.lVal());
