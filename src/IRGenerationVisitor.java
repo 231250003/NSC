5804d01c7bfc7e46acdf4e941bccb4be8faa3826
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-04 11:02:07 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:02:07 up 26 min,  1 user,  load average: 0.44, 0.42, 0.36 a2b87b9e1c509f06c968b28215cc8d7d8f9473f0

--- src/IRGenerationVisitor.java
+++ src/IRGenerationVisitor.java
@@ -32,10 +32,9 @@ public class IRGenerationVisitor extends   SysYParserBaseVisitor<LLVMValueRef> {
         if ((parent instanceof SysYParser.StmtContext)&& (((SysYParser.StmtContext)parent).WHILE() != null)) {
             SysYParser.StmtContext stmtCtx = (SysYParser.StmtContext) parent;
             ParseTree grandparent = stmtCtx.getParent();
+            System.err.println("crzsasa");
             if (grandparent instanceof SysYParser.StmtContext)
             {
-                System.err.println("crzsasa");
-
                 SysYParser.StmtContext whileStmt = (SysYParser.StmtContext) grandparent;
                 if (whileStmt.WHILE() != null) {
                     symbolTable.enterScope(true);
