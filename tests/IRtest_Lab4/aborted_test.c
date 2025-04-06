3af871f94020b5b96134731f4c9f70c1e3473a4d
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-06 19:03:44 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:03:44 up  6:01,  1 user,  load average: 0.79, 0.67, 0.46 2eadf30759e137cb4109f0de9997e332485a8d4

--- tests/IRtest_Lab4/aborted_test.c
+++ tests/IRtest_Lab4/aborted_test.c
@@ -1,7 +1,16 @@
 int x[2003];
 int main(){
     int x=5;
-    it y=100;
-    x=y+1;
+    int y=100;
+    if(x>=5){
+        if(x<6){
+            x=!x+x;
+        }
+        if(x>7)
+        {
+            x=x+1;
+        }
+        else x=x-1;
+    }
     return x;
 }
