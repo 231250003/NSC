9db90d7a11848dcd90c62442cccf5f5401295923
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-06 16:25:09 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:25:09 up  3:08,  1 user,  load average: 0.26, 0.37, 0.58 ec652a87ab127f6d11c9f0a0da96faaf4bcf7a0d

--- src/IRGenerationVisitor.java
+++ src/IRGenerationVisitor.java
@@ -158,7 +158,7 @@ public class IRGenerationVisitor extends   SysYParserBaseVisitor<LLVMValueRef> {
     public LLVMValueRef visitConstExp(SysYParser.ConstExpContext ctx) {
         return visit(ctx.exp());
     }
-    
+
     public LLVMValueRef getconstinitvalue(SysYParser.ConstInitValContext ctx) {
         if(ctx.L_BRACE()==null){
             return visit(ctx.constExp());
@@ -355,7 +355,17 @@ public class IRGenerationVisitor extends   SysYParserBaseVisitor<LLVMValueRef> {
     }
     @Override
     public LLVMValueRef visitCond(SysYParser.CondContext ctx) {
-        if(ctx.exp()!=null) return visit(ctx.exp());
+        if(ctx.exp()!=null){
+            LLVMValueRef val = visit(ctx.exp());
+            if (LLVMGetTypeKind(LLVMTypeOf(val)) == LLVMPointerTypeKind) {
+                val = LLVMBuildLoad(builder, val, "load_exp");
+            }
+            if (LLVMGetTypeKind(LLVMTypeOf(val)) != LLVMIntegerTypeKind ||
+                    LLVMGetIntTypeWidth(LLVMTypeOf(val)) != 1) {
+                val = LLVMBuildICmp(builder, LLVMIntNE, val, LLVMConstInt(LLVMTypeOf(val), 0, 0), "to_bool");
+            }
+            return val;return visit(ctx.exp());
+        }
         else if (ctx.LT() != null || ctx.GT() != null || ctx.LE() != null || ctx.GE() != null) {
             // 处理 <, >, <=, >=
             LLVMValueRef left = visit(ctx.cond(0));
