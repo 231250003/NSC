fc342d61c5b1319c16f67dfabffd1fb38129d419
231250003-Chen rui ze <crz@DESKTOP-LJJBL1C>
2025-04-06 20:26:35 +0800

    2700

--- src/semantic_check/SymbolTable.java
+++ src/semantic_check/SymbolTable.java
@@ -61,6 +61,7 @@ public class SymbolTable {
                     LLVMValueRef argValue =new LLVMValueRef(args.get(i)); // 获取 LLVM 参数值
                     param.reference = argValue; // 赋值给 symbol 的 reference
                 }
+                //crz
             }
         }
     }
