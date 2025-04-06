f267c5985241a21ea2ef2dcd2ec5e06c0094f04d
231250003-Chen rui ze <crz@DESKTOP-LJJBL1C>
2025-04-06 23:48:11 +0800

    crz

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
             System.err.println("input path is required");
         }*/
           String source = args[0];
-          if(args[0].contains("normaltest0"))  return;
+         // if(args[0].contains("normaltest0"))  return;
          CharStream input = CharStreams.fromFileName(source);
          SysYLexer lexer = new SysYLexer(input);
           lexer.removeErrorListeners();
