cad538647db5bc662c32e688ecfc01ccd6422936
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-06 23:03:55 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:03:55 up 10:22,  1 user,  load average: 1.70, 0.62, 0.52 3e77353746be5c4e3fb8e558de799ec3ca47e64

--- tests/IRtest_Lab4/aaa_2700_test.c
+++ tests/IRtest_Lab4/aaa_2700_test.c
@@ -2,7 +2,7 @@ int x = 100, y = 2, z = 3;
 int main() {
     if(x>=100){
         if(x<10) return 0;
-        else if(x>20) return 2;
+        else if(x>200) return 2;
         else if(x>0x100) return 3;
         x=x+1;
     }
