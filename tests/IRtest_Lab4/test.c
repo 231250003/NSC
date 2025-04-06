a577990b8a865c333a4467039b62bad6cd91f49d
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-06 14:25:11 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:25:11 up  1:05,  1 user,  load average: 0.43, 0.30, 0.30 cb9371d51b441423ce211b6fd36ea45dfbec05fe

--- tests/IRtest_Lab4/test.c
+++ tests/IRtest_Lab4/test.c
@@ -1,7 +1,15 @@
 int a = 10;
 int f(int x){
+    a=a+1;
     return a+10;
 }
 int main() {
-    return f(10);
+    a=a+1;
+    int t=f(a);
+    while(f(a)<10){
+        if(f(a)>10){
+            return a;
+        }
+    }
+    return a+1;
 }
