222efe2d81a9d02413d7fd78cd86f7c974c6b5f6
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-06 19:51:36 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:51:36 up  6:54,  1 user,  load average: 0.55, 0.61, 0.75 ba46498c4abd775aedbfcc204a7e2f64f2e98a2

--- src/IRGenerationVisitor.java
+++ src/IRGenerationVisitor.java
@@ -192,6 +192,10 @@ public class IRGenerationVisitor extends   SysYParserBaseVisitor<LLVMValueRef> {
                 argCount = ctx.funcRParams().param().size();
                 symbolTable.set_r_params(funcName,args);
             }
+            if(((FunctionType)(symbolTable.get_name_matched_symbol(funcName).type)).getReturnType() instanceof VoidType){
+                LLVMBuildCall(builder, function, args, argCount, funcName);
+                return null;
+            }
             return castToI32(LLVMBuildCall(builder, function, args, argCount, funcName));
         }
         else if (ctx.exp().size() == 1 && ctx.unaryOp() != null) {
