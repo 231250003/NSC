d7c561890d27502edd1ab787466153fb79c81926
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-06 18:24:34 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:24:34 up  5:18,  1 user,  load average: 1.60, 1.01, 0.63 27ddd82bb913e9fce6fda449638f2dc28ba91e4

--- tests/IRtest_Lab4/and_test.c
+++ tests/IRtest_Lab4/and_test.c
@@ -9,9 +9,10 @@ int main(){
     int y=10;
     z=y;
     if(x>50 || y/2==0){
+        int z=5;
         while(1){
-            g(2,3);
-            if(x<50){
+            int z=g(2,3);
+            if(x<50&&z>100){
                 x=2;
                 g(10,2);
                 continue;
