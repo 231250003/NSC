e78f3bae0e32a346c5445c00edf9ef8e3129eb36
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-06 14:08:28 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:08:28 up 46 min,  1 user,  load average: 0.77, 0.38, 0.31 505c2d30ea5a462c2fc432af1a3b9b032e98c62

---
+++ tests/IRtest_Lab4/while_test.c
@@ -0,0 +1,16 @@
+int a = 1;
+int main(int x) {
+    int b=0;
+    while(a<9){
+        while(a<10){
+        if(a==77) continue;
+        a=a+1;
+        b=a+1;
+        a=b+1;
+        break;
+        }
+        break;
+    }
+    x=x+1;
+    return x;
+}
