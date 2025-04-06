5d4fb6b01866a39245a1602f7182489fa2a86f7d
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-06 18:20:25 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:20:25 up  5:13,  1 user,  load average: 0.17, 0.34, 0.35 3c4ac15839b1b9073af18a3a56cbe47665f5d3

--- tests/IRtest_Lab4/while_test.sysy.c
+++ tests/IRtest_Lab4/while_test.sysy.c
@@ -1,23 +1,3 @@
-int x=10;
-int z;
-int g(int x,int y){
-    y=y+1;
-    z=z+1;
-    return y+1;
-}
 int main(){
-    int y=10;
-    z=y;
-    if(x>50 || y/2==0){
-        while(1){
-            g(2,3);
-            if(x<50){
-                x=2;
-                g(10,2);
-                continue;
-            }
-            if(x>100) break;
-        }
-    }
-    return x;
+    return -2;
 }
