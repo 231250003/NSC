8e8cf5ca050d40d03e189512d09a3a16e40e62a4
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-06 15:50:19 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:50:19 up  2:30,  1 user,  load average: 0.68, 0.75, 0.71 4adcf06f9cf0011afcefea94432ea75212882

--- tests/IRtest_Lab4/and_test.c
+++ tests/IRtest_Lab4/and_test.c
@@ -1,13 +1,15 @@
 int x=10;
-void g(){
-    x=x+1;
+const int z=20;
+void g(int x,int y){
+    y=y+1;
+    z=z+1;
     return;
 }
 int main(){
     int y=10;
     if(x>50 || y/2==0){
         while(1){
-            g();
+            g(2,3);
             if(x<50){
                 x=2;
                 g();
