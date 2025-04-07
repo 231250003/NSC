f559bed620f2e0f5ddf49ba70078e316ff5884f2
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-07 14:11:39 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:11:39 up  4:30,  1 user,  load average: 1.74, 0.57, 0.51 b685e67c19f54328771c92a37ca0e436fe60a827

--- tests/IRtest_Lab4/part4test.c
+++ tests/IRtest_Lab4/part4test.c
@@ -2,11 +2,6 @@ int a = 10;
 int main() {
     int b=2;
     int c=3;
-    if(a>b){
-        if(b>c){
-            a=a+1;
-        }
-        a=a+1;
-    }
+    if(!(b!=2&&c/0==0)) return a;
     return a;
 }
