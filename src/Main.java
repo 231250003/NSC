30b213b6239ba4828eb581c73f7f6fbe47223b7b
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-07 16:52:12 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:52:12 up 43 min,  1 user,  load average: 0.55, 0.82, 0.67 464ffdf31ee7f76259c370291af9f6bd2b59358a

--- src/Main.java
+++ src/Main.java
@@ -67,6 +67,16 @@ public class Main {
         IRGenerationVisitor visitor = new IRGenerationVisitor(module, builder);
         visitor.visit(tree);
        // LLVMDumpModule(module);
+        LLVMPassManagerRef passManager = LLVMCreatePassManager();
+        LLVMAddCFGSimplificationPass(passManager);        // 合并基本块、删除不可达代码、空块
+        LLVMAddAggressiveDCEPass(passManager);            // 更激进的删除死指令
+        LLVMAddDeadStoreEliminationPass(passManager);     // 删除不必要的 store
+        LLVMAddPromoteMemoryToRegisterPass(passManager);     // alloca -> register
+        LLVMAddInstructionCombiningPass(passManager);        // 折叠指令
+        LLVMAddReassociatePass(passManager);                 // 表达式重写
+        LLVMAddGVNPass(passManager);                         // 全局值编号消除冗余
+        LLVMRunPassManager(passManager, module);
+        LLVMDisposePassManager(passManager);
         BytePointer error = new BytePointer((Pointer) null);
         if (LLVMPrintModuleToFile(module, args[1], error) != 0) {
             LLVMDisposeMessage(error);
