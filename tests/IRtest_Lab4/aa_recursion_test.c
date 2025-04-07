8ac30eb7dc6255b1c9ff169ffbb11089c238df18
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-07 12:25:03 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:25:03 up  2:47,  1 user,  load average: 0.05, 0.21, 0.16 99071aa791bc9a8a9ffb685b426cef4e375b49dd

--- tests/IRtest_Lab4/aa_recursion_test.c
+++ tests/IRtest_Lab4/aa_recursion_test.c
@@ -1,26 +1,6 @@
-int g(int x){
- return x+1;
-}
-int f(int x){
- int f=10;
-  return f;
-}
-void x(){
-    return;
-}
-void t(){
-    return;
-}
+int f(int x) { return x + 1; }
+
 int main() {
-    int main=f(6);
-    int f=20;
-    int x=10;
-    f+(1);
-    main=2*main%10+10+f(1)+f+((f)+(f))+(g((((f(f(f)))))));
-    f(f);
-    f(f);
-    f(f);
-    f(f);
-    int z;
-    return main+f(f);
+    int a = 1;
+    return f(a);
 }
