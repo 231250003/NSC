8ffb911a794dc70899e8f5db33f648556eff7a9f
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-30 16:17:19 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:17:19 up  4:45,  1 user,  load average: 0.24, 0.11, 0.09 dae90330f69c1f8e590e5e9e9b569fa2ee07e77c

--- src/Main.java
+++ src/Main.java
@@ -80,7 +80,7 @@ public class Main {
         if (LLVMPrintModuleToFile(module, args[1], error) != 0) {
             LLVMDisposeMessage(error);
         }
-        LLVMIRToRiscv llvmirToRiscv=new LLVMIRToRiscv(module,args[1].substring(0,args[1].length()-3)+"riscv");
+        LLVMIRToRiscv llvmirToRiscv=new LLVMIRToRiscv(module,args[1].substring(0,args[1].length()-3)+".riscv");//TODO need to be changed when submitted
         llvmirToRiscv.to_riscv();
         LLVMDisposeBuilder(builder);
         LLVMDisposeModule(module);
