939ec7bb2df19763fa54e9e5922bfb50296a0c96
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-01 16:59:28 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:59:28 up  8:12,  0 users,  load average: 0.56, 0.31, 0.17 b07975aa45b9d02a14825ab379c933824418c039

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -121,7 +121,6 @@ public class LLVMIRToRiscv {
     }
 
     private String evaluate(LLVMValueRef val) {
-        System.out.println(LLVM.LLVMGetValueName(val).getString());
         LLVMValueRef constInt = LLVM.LLVMIsAConstantInt(val);
         if (constInt != null && !constInt.isNull()){
             long imm = LLVM.LLVMConstIntGetSExtValue(constInt);
@@ -141,6 +140,7 @@ public class LLVMIRToRiscv {
             return reg;
         }
         else {
+            System.out.println(LLVM.LLVMGetValueName(val).getString());
             String valStr = LLVM.LLVMPrintValueToString(val).getString();
             int kind = LLVM.LLVMGetValueKind(val);
             System.err.println("Unsupported operand:");
