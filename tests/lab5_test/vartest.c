016c7e2e5cb929f7d3d4fe59862f3a5caf0b06e8
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-01 18:23:53 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:23:53 up  9:35,  0 users,  load average: 0.33, 0.22, 0.08 614a94d4dcac618135592873468e5e53996c497c

--- tests/lab5_test/vartest.c
+++ tests/lab5_test/vartest.c
@@ -6,7 +6,6 @@ int main(){
     z=(2+5%3);
     int p=-2;
     int q=!10;
-    b = c + a + 1+!!cccc;
     const int d = 10+ a + b + c + d+0x10+z+p+q+dddd;
     return a + b + c + d+0x10+z+p+q+dddd;
 }
