c0dfa68e057c9afd5a793b530f7d7a9968a1f53d
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-02 16:07:35 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:07:35 up  7:43,  1 user,  load average: 0.81, 0.64, 0.34 a0a2e7fa916cc3923feeaac7e8c77067a3275d9

--- src/LinearScanRegisterAllocator.java
+++ src/LinearScanRegisterAllocator.java
@@ -50,6 +50,7 @@ class LinearScanRegisterAllocator implements RegisterAllocator {
     public void expireOldIntervals(int currentLine) {
         active.removeIf(interval -> {
             if (interval.end >= currentLine) return false;
+            System.out.println("crzzz");
             String loc = varToLocation.get(interval.varName);
             if (!loc.contains("sp")) {
                 registers.add(loc);
