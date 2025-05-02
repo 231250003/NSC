ad01d3572841d1f95774904033e82b19dfa08a9d
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-02 15:06:59 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:06:59 up  6:41,  1 user,  load average: 0.48, 0.30, 0.22 96cd3cd7428a864c4d0f48c332c4b472297d8d2e

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -140,7 +140,7 @@ public class LLVMIRToRiscv {
                                 throw new RuntimeException("Unsupported binop");
                         }
                         asm.op2(op, destReg, reg1, reg2);
-                        System.out.println(getValueKey(inst));
+                        System.out.println(LLVM.LLVMGetValueName(inst).getString());
                         String addr = allocator.allocate(getValueKey(inst).toString());
                         if(addr.contains("sp")) asm.instr("sw", destReg, addr);
                         else asm.instr("mv",addr,destReg);
