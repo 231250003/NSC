7f00e0073ea707bb4065e24a49d97c8c7d82e3ed
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-02 14:20:13 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:20:13 up  5:54,  1 user,  load average: 0.88, 0.45, 0.27 9e7bce5552ef6a2dd32141828911f565ad2405b9

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -87,17 +87,17 @@ public class LLVMIRToRiscv {
                         String addr = valueMap.get(LLVM.LLVMGetValueName(ptr).getString());
                         if(addr==null) {
                             String reg = freshReg();
-                            asm.instr("lw", reg, addr);
-                            //valueMap.put(LLVM.LLVMGetValueName(ptr).getString(), addr);  // 可选：也可以保存为 reg
+                            asm.instr("la",reg,(LLVM.LLVMGetValueName(ptr).getString()));
+                            asm.instr("lw",reg,"0("+reg+")");
                         }
                         else if(addr.contains("sp")){
                             String reg = freshReg();
-                            asm.instr("la",reg,(LLVM.LLVMGetValueName(ptr).getString()));
-                            asm.instr("lw",reg,"0("+reg+")");
                             String lval_addr = allocator.allocate(LLVM.LLVMGetValueName(inst).getString());
+                            asm.instr("lw",reg,addr);
                             if(lval_addr.contains("sp")) asm.instr("sw",reg,lval_addr);
                             else asm.instr("mv",lval_addr,reg);
                             valueMap.put(LLVM.LLVMGetValueName(inst).getString(), lval_addr);
+                            //valueMap.put(LLVM.LLVMGetValueName(ptr).getString(), addr);  // 可选：也可以保存为 reg
                         }
                         else{
                             String lval_addr = allocator.allocate(LLVM.LLVMGetValueName(inst).getString());
