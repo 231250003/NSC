e721d495fd1428889789c8237ffe1fd79454e660
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-02 16:06:45 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:06:45 up  7:42,  1 user,  load average: 0.85, 0.57, 0.30 88e2a7cdb7072da7d1f0140fc41ac47b8a9b3a65

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -45,7 +45,7 @@ public class LLVMIRToRiscv {
         }
         List<String> reg_list=new ArrayList<>();
         for(int i=0;i<32;i++){
-            if(i!=2&&i!=5&&i!=6&&i!=7) reg_list.add("x"+i);
+            if(i!=0&&i!=2&&i!=5&&i!=6&&i!=7) reg_list.add("x"+i);
         }
         allocator = new LinearScanRegisterAllocator(intervals, reg_list);
         //allocator=new StackOnlyRegisterAllocator();
