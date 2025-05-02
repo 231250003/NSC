7616b1136baef0b99ecdc43145505b70be16735a
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-02 14:52:58 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:52:58 up  6:27,  1 user,  load average: 0.16, 0.21, 0.18 2f45466ed3e21085496a5e79b747b8281a669fb

--- src/LinearScanRegisterAllocator.java
+++ src/LinearScanRegisterAllocator.java
@@ -13,10 +13,10 @@ class LinearScanRegisterAllocator implements RegisterAllocator {
         this.registers = new ArrayList<>(registers);
         this.varToInterval = new HashMap<>();
         for (Interval interval : allIntervals) {
-            System.out.println("interval");
-            System.out.println(interval.varName);
-            System.out.println(interval.start);
-            System.out.println(interval.end);
+//            System.out.println("interval");
+//            System.out.println(interval.varName);
+//            System.out.println(interval.start);
+//            System.out.println(interval.end);
             varToInterval.put(interval.varName, interval);
         }
     }
@@ -27,7 +27,8 @@ class LinearScanRegisterAllocator implements RegisterAllocator {
 //        System.out.println(lineNumber);
 //        System.out.println("variable:");
         for (String var : LLVMIRToRiscv.extractVariables(instruction)) {
-            System.out.println(var);
+            //System.out.println(var);
+            if(var.equals("load_lval")) System.out.println("crz");
             Interval interval = varToInterval.get(var);
             if (interval == null || varToLocation.containsKey(var)) continue;
 
