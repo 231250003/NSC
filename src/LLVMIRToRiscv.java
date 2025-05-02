f6be73ce36f6aa48a9fb6a61b1109473c1c4d51a
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-02 15:03:07 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:03:07 up  6:37,  1 user,  load average: 0.18, 0.13, 0.16 8dc4754e79c8bb451f8f530b46dd978d69f65d4b

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -86,14 +86,14 @@ public class LLVMIRToRiscv {
                         LLVMValueRef ptr = LLVM.LLVMGetOperand(inst, 0);
                         String addr = valueMap.get(LLVM.LLVMGetValueName(ptr).getString());
                         if(addr==null) {
+                            System.out.println("LLVMTOIR");
+                            System.out.println(LLVM.LLVMGetValueName(inst).getString());
                             String reg = freshReg();
                             asm.instr("la",reg,(LLVM.LLVMGetValueName(ptr).getString()));
                             asm.instr("lw",reg,"0("+reg+")");
                             String lval_addr = allocator.allocate(LLVM.LLVMGetValueName(inst).getString());
                             if(lval_addr.contains("sp")) asm.instr("sw",reg,lval_addr);
                             else asm.instr("mv",lval_addr,reg);
-                            System.out.println("LLVMTOIR");
-                            System.out.println(LLVM.LLVMGetValueName(inst).getString());
                             valueMap.put(LLVM.LLVMGetValueName(inst).getString(), lval_addr);
                         }
                         else if(addr.contains("sp")){
