39431bf90558ff6d6ffc467d68ccef804b610250
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-02 16:20:25 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:20:25 up  7:56,  1 user,  load average: 1.45, 0.90, 0.56 37d6e10eda9c390cc4a4dab1bde3b3aee909f7b1

--- src/LinearScanRegisterAllocator.java
+++ src/LinearScanRegisterAllocator.java
@@ -51,12 +51,10 @@ class LinearScanRegisterAllocator implements RegisterAllocator {
         for (Map.Entry<String, Interval> entry : varToInterval.entrySet()) {
             String var = entry.getKey();
             Interval interval = entry.getValue();
-            System.out.println(currentLine);
-            System.out.println(interval.end);
-            System.out.println("crzzzz");
             if(currentLine>interval.end && varToLocation.containsKey(var)&&(!varToLocation.get(var).contains("sp"))) {
                 registers.add(varToLocation.get(var));
                 varToLocation.remove(var);
+                System.out.println("crzzzz");
             }
         }
 //        active.removeIf(interval -> {
