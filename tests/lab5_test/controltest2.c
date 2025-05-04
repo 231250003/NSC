17a057a64200523c2d46793be3701561b29c7e91
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-04 20:07:17 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:07:17 up  5:29,  1 user,  load average: 1.01, 0.77, 0.83 1e37117110cd871035ea018ae5f2771073cfca05

--- tests/lab5_test/controltest2.c
+++ tests/lab5_test/controltest2.c
@@ -13,8 +13,7 @@ int main() {
          }
     }
     while (b+a>20) {
-        if(a!=56) return 1;
-        else return !a+b;
+       
         while(a>10){
             if(a>40){
              a=a-5;
