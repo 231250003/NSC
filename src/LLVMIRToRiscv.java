5be9aba175ca12317d72dd8f3e57114f4817e8bd
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-04 19:32:21 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:32:21 up  4:53,  1 user,  load average: 1.73, 0.62, 0.34 41b7a4139c52de2d680d45975c2796484d43ca92

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -219,6 +219,7 @@ public class LLVMIRToRiscv {
                             String condReg = evaluate(cond);
                             String trueLabel = LLVM.LLVMGetValueName(ifTrue).getString();
                             String falseLabel = LLVM.LLVMGetValueName(ifFalse).getString();
+                            System.out.println(cond);
                             System.out.println(trueLabel);
                             System.out.println(falseLabel);
                             asm.bnez(condReg, trueLabel);
