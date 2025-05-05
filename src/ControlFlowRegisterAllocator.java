7ceb14c91155e480739189134dcc73fcf0ba4c09
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-05 22:34:24 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:34:24 up  1:03,  1 user,  load average: 0.33, 0.15, 0.28 9ef6d8a24b89d30ce97092aff4905dd89f3d3021

--- src/ControlFlowRegisterAllocator.java
+++ src/ControlFlowRegisterAllocator.java
@@ -26,8 +26,7 @@ class ControlFlowRegisterAllocator implements RegisterAllocator {
     }
 
     public void processInstruction(int lineNumber, String instruction) {
-        expireOldIntervals(lineNumber);
-        Map<String,Integer> used_reg_list=new HashMap<>();
+        //expireOldIntervals(lineNumber);
         for (String var : LLVMIRToRiscv.extractVariables(instruction)) {
             Interval interval = varToInterval.get(var);
             if (interval == null || varToLocation.containsKey(var)) continue;
@@ -35,47 +34,25 @@ class ControlFlowRegisterAllocator implements RegisterAllocator {
                 String reg = registers.remove(0);
                 varToLocation.put(var, reg);
             } else {
-                int var_end_line=interval.end;
-                String spill_reg="";
-                String var_spill_name=var;
-                for (Map.Entry<String, Interval> entry : varToInterval.entrySet()) {
-                    String key = entry.getKey();
-                    Interval value = entry.getValue();
-                    if(varToLocation.get(key)!=null&&(!varToLocation.get(key).contains("sp"))&&value.end>var_end_line&&used_reg_list.get(varToLocation.get(key))==null){
-                        spill_reg=varToLocation.get(key);
-                        var_end_line=value.end;
-                        var_spill_name=key;
-                    }
+                if (!varOffset.containsKey(var)) {
+                    nextOffset += 4;
+                    varOffset.put(var, nextOffset);
                 }
-                if(!spill_reg.isEmpty()){
-                    if (!varOffset.containsKey(var_spill_name)) {
-                        nextOffset += 4;
-                        varOffset.put(var_spill_name, nextOffset);
-                    }
-                    varToLocation.put(var_spill_name, String.format("%d(sp)", varOffset.get(var_spill_name)));
-                    LLVMIRToRiscv.valueMap.put(var_spill_name,varToLocation.get(var_spill_name));
-                    asmBuilder.instr("sw",spill_reg,varToLocation.get(var_spill_name));
-                    used_reg_list.put(spill_reg,1);
-                    if (varOffset.containsKey(var)) {
-                        asmBuilder.instr("lw", spill_reg, String.format("%d(sp)", varOffset.get(var)));
-                    }
-                    varToLocation.put(var,spill_reg);
-                    LLVMIRToRiscv.valueMap.put(var,spill_reg);
-                }
-                else
-                {
-                    if (!varOffset.containsKey(var)) {
-                        nextOffset += 4;
-                        varOffset.put(var, nextOffset);
-                    }
-                    varToLocation.put(var, String.format("%d(sp)", varOffset.get(var)));
-                    LLVMIRToRiscv.valueMap.put(var,varToLocation.get(var));
-                }
+                varToLocation.put(var, String.format("%d(sp)", varOffset.get(var)));
+                LLVMIRToRiscv.valueMap.put(var,varToLocation.get(var));
             }
         }
     }
 
     public void expireOldIntervals(int currentLine) {
+        for (Map.Entry<String, Interval> entry : varToInterval.entrySet()) {
+            String var = entry.getKey();
+            Interval interval = entry.getValue();
+            if(currentLine>interval.end && varToLocation.containsKey(var)&&(!varToLocation.get(var).contains("sp"))) {
+                registers.add(varToLocation.get(var));
+                varToLocation.remove(var);
+            }
+        }
     }
 
     @Override
