94682c3f1593c915d8325daacfd1a4cc396a49f4
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-05 16:47:29 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:47:29 up  4:01,  1 user,  load average: 0.23, 0.54, 0.40 c420c524adaf012e728fc52337d3de7cd4e281e9

--- src/LinearScanRegisterAllocator.java
+++ src/LinearScanRegisterAllocator.java
@@ -49,6 +49,7 @@ class LinearScanRegisterAllocator implements RegisterAllocator {
                     }
                     varToLocation.put(var_spill_name, String.format("%d(sp)", varOffset.get(var_spill_name)));
                     asm.instr("sw",spill_reg,varToLocation.get(var_spill_name));
+                    System.out.println(spill_reg);
                     used_reg_list.put(spill_reg,1);
                     if (varOffset.containsKey(var)) {
                         asm.instr("lw", spill_reg, String.format("%d(sp)", varOffset.get(var)));
