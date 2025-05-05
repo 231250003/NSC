8b0ce6f44d460192018b51e6c52f8416398e5231
231250003-Chen rui ze <crz@DESKTOP-LJJBL1C>
2025-05-05 17:25:19 +0800

    crz

--- src/LinearScanRegisterAllocator.java
+++ src/LinearScanRegisterAllocator.java
@@ -51,9 +51,9 @@ class LinearScanRegisterAllocator implements RegisterAllocator {
                     varToLocation.put(var_spill_name, String.format("%d(sp)", varOffset.get(var_spill_name)));
                     LLVMIRToRiscv.valueMap.put(var_spill_name,varToLocation.get(var_spill_name));
                     asm.instr("sw",spill_reg,varToLocation.get(var_spill_name));
-                    System.out.println(var);
-                    System.out.println(var_spill_name);
-                    System.out.println(spill_reg);
+//                    System.out.println(var);
+//                    System.out.println(var_spill_name);
+//                    System.out.println(spill_reg);
                     used_reg_list.put(spill_reg,1);
                     if (varOffset.containsKey(var)) {
                         asm.instr("lw", spill_reg, String.format("%d(sp)", varOffset.get(var)));
