76dd7784a4d454970e0326b2b0106a7b0f683eb9
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-05 23:24:49 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:24:49 up  1:57,  1 user,  load average: 0.29, 0.59, 0.56 222f61d4794743f6b00889da87332e1d63e5b9b2

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -63,6 +63,7 @@ public class LLVMIRToRiscv {
                         varsInBlock.add(var);
                     }
                     lineNum++;
+                    System.out.println(line);
                 }
                 blockVars.put(bb, varsInBlock);
                 block_last_num.put(LLVM.LLVMGetBasicBlockName(bb).getString(),lineNum);
