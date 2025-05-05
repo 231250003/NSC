262cb3a7301536f64434c218772a441ca9851945
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-05 14:14:51 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:14:51 up  1:19,  1 user,  load average: 1.96, 1.38, 1.08 9d33a25ef585b53f7f69fe71f0f21c475b9d11d

--- src/ControlFlowRegisterAllocator.java
+++ src/ControlFlowRegisterAllocator.java
@@ -34,12 +34,15 @@ class ControlFlowRegisterAllocator implements RegisterAllocator {
             if (interval == null || varToLocation.containsKey(var)) continue;
             boolean isInTop27 = false;
             int limit = Math.min(27, sortedEntries.size());
+            System.out.println(var);
             for (int i = 0; i < limit; i++) {
+                System.out.println(sortedEntries.get(i).getKey());
                 if (sortedEntries.get(i).getKey().equals(var)) {
                     isInTop27 = true;
                     break;
                 }
             }
+            System.out.println("end of line");
             if (!registers.isEmpty()&&isInTop27) {
                 System.out.println("crzzz");
                 String reg = registers.remove(0);
