8ada022fa3bbed2784d2d1acc37afa0405f15e39
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-06 13:19:29 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:19:29 up 32 min,  1 user,  load average: 1.99, 0.72, 0.33 ecd18b991ed1602c3a7c44c882462fb4db9c0485

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -146,7 +146,7 @@ public class LLVMIRToRiscv {
             if (!"main".equals(funcName)) continue;
             asm.label("main");
             // Prologue
-            int stackSize = 1024;
+            int stackSize = 2048;
             asm.instr("addi", "sp", "sp", "-" + stackSize);
             for (LLVMBasicBlockRef bb = LLVM.LLVMGetFirstBasicBlock(func); bb != null && !bb.isNull(); bb = LLVM.LLVMGetNextBasicBlock(bb)) {
                // System.out.println("crzzzzzzzz");
