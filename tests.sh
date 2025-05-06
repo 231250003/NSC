acbb8f45debd8f45e69bed981a4f7bb4ce5afafb
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-06 17:08:10 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:08:10 up  1:39,  1 user,  load average: 2.08, 0.70, 0.35 97d6c2917ade3eae24b20c6944448de7859e5a9

--- tests.sh
+++ tests.sh
@@ -5,7 +5,6 @@ echo "== Batch processing all .sysy files in $SYSY_DIR =="
 
 for sysy_file in "$SYSY_DIR"/*.sysy; do
     filename=$(basename "$sysy_file" .sysy)
-    filename="runtimeerror"
     c_file="$SYSY_DIR/$filename.c"
     exe_file="$SYSY_DIR/$filename"
     ll_file="$SYSY_DIR/$filename.ll"
