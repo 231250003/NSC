103d87f55ddb4fd9203a4ee6737cf9e0e6b02fd9
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-06 09:40:17 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:40:17 up  1:31,  1 user,  load average: 1.44, 0.88, 0.46 34bf9f74c425d20fce89985a2f144e1dd68e6256

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -86,8 +86,8 @@ public class LLVMIRToRiscv {
                     if(line.contains("br")&&var.contains(LLVM.LLVMGetBasicBlockName(bb).getString())&&block_last_num.get(LLVM.LLVMGetBasicBlockName(bb2).getString())>finalLineNum){
                         finalLineNum=block_last_num.get(LLVM.LLVMGetBasicBlockName(bb2).getString());
 //                        System.out.println(LLVM.LLVMGetBasicBlockName(bb).getString());
-//                        System.out.println(LLVM.LLVMGetBasicBlockName(bb2).getString());
-//                        System.out.println("crzzzz");
+//                       System.out.println(LLVM.LLVMGetBasicBlockName(bb2).getString());
+//                       System.out.println("crzzzz");
                     }
                 }
             }
@@ -106,9 +106,9 @@ public class LLVMIRToRiscv {
                     Set<String> var=extractVariables(line2);
                     if(line.contains("br")&&var.contains(LLVM.LLVMGetBasicBlockName(bb).getString())&&block_first_num.get(LLVM.LLVMGetBasicBlockName(bb2).getString())<first_line_num){
                         first_line_num=block_first_num.get(LLVM.LLVMGetBasicBlockName(bb2).getString());
-//                        System.out.println(LLVM.LLVMGetBasicBlockName(bb).getString());
-//                        System.out.println(LLVM.LLVMGetBasicBlockName(bb2).getString());
-//                        System.out.println("crzzzz");
+                        System.out.println(LLVM.LLVMGetBasicBlockName(bb).getString());
+                        System.out.println(LLVM.LLVMGetBasicBlockName(bb2).getString());
+                       System.out.println("crzzzz");
                     }
                 }
             }
