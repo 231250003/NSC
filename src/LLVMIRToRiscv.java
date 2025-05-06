597f554efeba46ad9d81e8894235ea105040ae46
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-06 21:49:36 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:49:36 up  4:16,  1 user,  load average: 0.57, 1.02, 1.09 af338c06659123bfabc25d93907e7c03d48d76f6

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -327,8 +327,9 @@ public class LLVMIRToRiscv {
                     }
                 }
                 if(blockCount>1) NewControlFlowRegisterAllocator.post_process_block();
+                System.out.println(lineNum);
+                System.out.println("ccrzzz");
             }
-            System.out.println(lineNum);
         }
         asm.writeToFile(file_path);
     }
