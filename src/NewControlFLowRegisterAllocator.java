9ecca3185d39ed014cb2540d3d392e470b57b47b
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-06 21:43:08 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:43:08 up  4:09,  1 user,  load average: 1.89, 1.70, 1.20 1c7fe65086eb4560b3612b76ea9b9506c79ad22e

--- src/NewControlFLowRegisterAllocator.java
+++ src/NewControlFLowRegisterAllocator.java
@@ -63,7 +63,6 @@ class NewControlFlowRegisterAllocator implements RegisterAllocator{
         }
         for (Interval interval : intervals) {
             varToInterval.put(interval.varName, interval);
-            System.out.println(interval.end);
         }
     }
     public  void processInstruction(int lineNumber, String instruction) {
