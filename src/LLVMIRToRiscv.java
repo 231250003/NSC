e058aeb887d6b7cb0f867ca308f626134a01306b
231250003-Chen rui ze <crz@DESKTOP-LJJBL1C>
2025-05-06 13:59:04 +0800

    crz

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -127,7 +127,7 @@ public class LLVMIRToRiscv {
         }
         List<String> reg_list=new ArrayList<>();
         for(int i=0;i<32;i++){
-            if(i!=0&&i!=2&&i!=5&&i!=6&&i!=7&&i!=10&&i!=17) reg_list.add("x"+i);
+            if(i!=0&&i!=2&&i!=5&&i!=6&&i!=7) reg_list.add("x"+i);
             //if(i==10&&i==11&&i==12&&i==15&&i==16) reg_list.add("x"+i);
         }
         allocator = new LinearScanRegisterAllocator(intervals, reg_list,asm);
