f02e4bb96814ec10a2572daa85b7326236670272
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-06 17:07:38 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:07:38 up  1:39,  1 user,  load average: 0.04, 0.14, 0.16 b836dc78f893fd96b599e8e4242da27f681b53f

--- tests/lab5_test/runtimeerror.c
+++ tests/lab5_test/runtimeerror.c
@@ -1,16 +1,53 @@
-const int a = 1,dddd=111;
-int b,cccc;
-int main(){
-  /*
-    int c = 3;
-    int z;
-    z=(2+5%3);
-    int p=-2;
-    int q=!10;
-    */
-    //b = c + a + 1+!!cccc;
-    //const int d = 10+ a + b + c +0x10+z+p+q+dddd;
-    //return a + b + c +d+0x10+z+p+q+dddd;
-    b = a + 1+!!cccc;
-    return b;
+int x = 56;
+int y = 98;
+
+int main() {
+    int a = x;
+    int b = y;
+     if(!a+b>=10) {
+        if(a<=10) return 2;
+     }
+    while(1){
+        a=a-2;
+        if(a==45) break;
+        else if(a==48){
+            b=b-3;
+            a=a+1;
+         }
+    }
+    while (b+a>20) {
+
+        while(a>10){
+            if(a>40){
+             a=a-5;
+                        break;
+              }
+             else break;
+        }
+        while(b>35){
+            {
+                int b=30;
+                {
+                    while(1){
+                        if(b<20)return b;
+                        else b=b-7;
+                        break;
+                    }
+                }
+            }
+            return b-10;
+            if(b>70){
+                b=b-5;
+                continue;
+            }
+            else if(b>50) b=b-7;
+            else if(b>30) b=b-1;
+            else return 10;
+            }
+        b=b-3;
+        a=a+1;
+        if(a+b>50) a=a-10;
+    }
+    int result = a;
+	return result;
 }
