e818b22e71dc8d33dbc54f644798e3035e7f9862
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-06 21:03:23 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:03:23 up  3:25,  1 user,  load average: 1.93, 0.85, 0.65 e4732b6bae28535dddf5e8f7164a5586908c9621

--- tests/lab5_test/controltest3.c
+++ tests/lab5_test/controltest3.c
@@ -2,6 +2,15 @@ int x = 56;
 int y = 98;
 
 int main() {
-    if(x>y) return x+y;
-    else return y;
+    int a = x;
+    int b = y;
+    while (b != 0) {
+           if (a > b) {
+               a = a - b;
+           } else {
+               b = b - a;
+           }
+       }
+    if(a!=!x+3) a=a+1;
+	return a;
 }
