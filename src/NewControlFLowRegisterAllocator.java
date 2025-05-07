7cb3f41c89662b43e3dbe6b2d8d21e1b8bbe3ffc
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-07 16:20:25 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:20:25 up  2:10,  1 user,  load average: 0.45, 1.02, 0.93 51a8e2893fd7803a3c08411bd17718dd2011ed96

--- src/NewControlFLowRegisterAllocator.java
+++ src/NewControlFLowRegisterAllocator.java
@@ -70,7 +70,7 @@ class NewControlFlowRegisterAllocator implements RegisterAllocator{
         }
     }
     public static boolean is_live_variable(LLVMBasicBlockRef bb,String var,boolean is_detecting_block){
-        if(variable_use_in_block.get("while.cond").contains("b")) System.out.println("contains are correct");
+        if(var.equals("b")&&LLVM.LLVMGetBasicBlockName(bb).getString().equals("while.cond")) System.out.println("crzzzzssdsds");
         if(visited_block.contains((LLVM.LLVMGetBasicBlockName(bb).getString()))){
             if(variable_use_in_block.get(LLVM.LLVMGetBasicBlockName(bb).getString()).contains(var)) return true;
             else return false;
