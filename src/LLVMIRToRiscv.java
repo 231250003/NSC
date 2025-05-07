d9bce17425defaedf60714c0d49cd6aac0d0121e
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-07 16:51:37 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:51:37 up  2:44,  1 user,  load average: 1.04, 0.68, 0.67 959d41b2f52e74495ed895045692fad47a71c74d

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -172,8 +172,11 @@ public class LLVMIRToRiscv {
                         LLVMValueRef ptr = LLVM.LLVMGetOperand(inst, 1);
                         String valReg = evaluate(val);
                         String addr = valueMap.get(LLVM.LLVMGetValueName(ptr).getString());
-                        if(valReg.equals("x1")&&addr.contains("16")) System.out.println(LLVM.LLVMGetValueName(inst).getString());
-                        if(valReg.equals("x9")&&addr.contains("16")) System.out.println(LLVM.LLVMGetValueName(inst).getString());
+                        if(valReg.contains("x1")&&addr.contains("16")) System.out.println(LLVM.LLVMGetValueName(inst).getString());
+                        if(valReg.contains("x9")&&addr.contains("16")) {
+                            System.out.println(LLVM.LLVMGetValueName(inst).getString());
+                            System.out.println("crzzzzz");
+                        }
                         if(addr!=null){
                             if(addr.contains("sp")) asm.instr("sw", valReg, addr);
                             else asm.instr("mv",addr,valReg);
