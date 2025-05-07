ed717b6c1046ae2fca09a883b4d3b90a79b11e4b
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-07 16:43:09 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:43:09 up  2:35,  1 user,  load average: 1.72, 0.85, 0.72 ccea4bc25b4d62330857c262ed98799eb9628a1

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -308,7 +308,7 @@ public class LLVMIRToRiscv {
                         if (numOperands == 1) {
                             LLVMValueRef dest = LLVM.LLVMGetOperand(inst, 0);
                             String loop_label = LLVM.LLVMGetValueName(dest).getString();
-                            NewControlFlowRegisterAllocator.post_process_block(LLVM.LLVMPrintValueToString(inst).getString());
+                            //NewControlFlowRegisterAllocator.post_process_block(LLVM.LLVMPrintValueToString(inst).getString());
                             asm.j(loop_label);
                         } else if (numOperands == 3) {
                             //System.out.println(LLVM.LLVMPrintValueToString(inst).getString());
@@ -322,7 +322,7 @@ public class LLVMIRToRiscv {
 //                            System.out.println(trueLabel);
 //                            System.out.println(falseLabel);
                             asm.bnez(condReg, trueLabel);
-                            //NewControlFlowRegisterAllocator.post_process_block(LLVM.LLVMPrintValueToString(inst).getString());
+                            NewControlFlowRegisterAllocator.post_process_block(LLVM.LLVMPrintValueToString(inst).getString());
                             asm.j(falseLabel);
                         }
                     }
