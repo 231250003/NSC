206e9619107ec2f200326ec9f4b11091cf0dc13b
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-08 16:31:52 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:31:52 up  4:17,  1 user,  load average: 1.98, 0.99, 0.44 1b5a8f3650b918e5cd9a9ca78c738edc78b10e8

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ import org.bytedeco.llvm.global.LLVM;
 import static org.bytedeco.llvm.global.LLVM.*;
 public class Main {
     public static boolean is_run_time_error_test=false;
-    public static boolean used_interpreter=false;
+    public static boolean used_interpreter=true;
     public static void main(String[] args) throws IOException {
         /* if (args.length < 1) {
             System.err.println("input path is required");
