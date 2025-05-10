b9970a35b8385c6a1bec1fde9794ef753a4a606a
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-10 23:24:26 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:24:26 up  1:12,  1 user,  load average: 2.11, 1.48, 0.78 3bc0a12bb782a2a12830d4c8d1a56d0fc9e5839a

--- src/AsmBuilder.java
+++ src/AsmBuilder.java
@@ -46,10 +46,11 @@ public class AsmBuilder {
             LLVMIRToRiscv.is_offset_init.put(Integer.parseInt(addr.substring(0,addr.indexOf("("))),false);
         }
         else if(op.equals("lw")){
-            System.out.println("crzzzz");
             String addr=args[1];
             int offset=Integer.parseInt(addr.substring(0,addr.indexOf("(")));
+            System.out.println(offset);
             if(offset!=0&&LLVMIRToRiscv.is_offset_init.get(offset)==false){
+                System.out.println("crzzzz");
                 current.append("  "+"sw"+"  "+"x0,"+"  "+addr);
                 current.append("\n");
             }
