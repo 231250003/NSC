7aa30086c6f3616d36a08554a0ad0499d51eef0d
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-21 09:59:07 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:59:07 up  1:46,  1 user,  load average: 0.44, 0.49, 0.41 c6b6681a94d55da350363c59a8796382455f3354

--- src/LLVMIROptimization.java
+++ src/LLVMIROptimization.java
@@ -161,7 +161,6 @@ public class LLVMIROptimization {
                 String src1 = LLVM.LLVMGetValueName(LLVM.LLVMGetOperand(inst, 0)).getString();
                 String src2 = LLVM.LLVMGetValueName(LLVM.LLVMGetOperand(inst, 1)).getString();
                 String dest = LLVM.LLVMGetValueName(inst).getString();
-                if(src1.isEmpty()||src2.isEmpty()) System.out.println("crzzz");
                 if (in_inst.get(inst).get(src1).getKind() == ConstPropValueHolder.Kind.INT &&
                         in_inst.get(inst).get(src2).getKind() == ConstPropValueHolder.Kind.INT) {
                     int x1 = in_inst.get(inst).get(src1).getIntValue();
