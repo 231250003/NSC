6c30e3328e37b062f9c0b0818145a235e5250a91
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-21 11:25:27 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:25:27 up  3:16,  1 user,  load average: 1.00, 1.20, 0.80 340f7aa23c3ed8f493a76b53d945108b66714b4

--- src/LLVMIROptimization.java
+++ src/LLVMIROptimization.java
@@ -287,13 +287,13 @@ public class LLVMIROptimization {
                     }
                 }
             }
-            System.out.println(LLVM.LLVMPrintValueToString(inst).getString());
-            for(Map.Entry<String,ConstPropValueHolder> entry:new_out.entrySet()){
-                System.out.print(entry.getKey());
-                System.out.print("  ");
-                System.out.println(entry.getValue());
-            }
-            System.out.println("--------------");
+//            System.out.println(LLVM.LLVMPrintValueToString(inst).getString());
+//            for(Map.Entry<String,ConstPropValueHolder> entry:new_out.entrySet()){
+//                System.out.print(entry.getKey());
+//                System.out.print("  ");
+//                System.out.println(entry.getValue());
+//            }
+//            System.out.println("--------------");
             out_inst.put(inst, new_out);
         }
        Set<String> is_constant=new HashSet<>();
