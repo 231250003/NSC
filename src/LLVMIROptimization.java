e3729b70794722bfa8975efd73e4e2efabf26bd4
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-21 14:53:58 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:53:58 up  6:26,  1 user,  load average: 0.49, 0.63, 0.77 1b0147c72f439865d7bc09965ba0051fa64c0da

--- src/LLVMIROptimization.java
+++ src/LLVMIROptimization.java
@@ -316,7 +316,7 @@ public class LLVMIROptimization {
                 }
             }
         }
-        for(String x:is_constant) System.out.println(x);
+//        for(String x:is_constant) System.out.println(x);
         Set<LLVMValueRef> inst_to_delete=new HashSet<>();
         for (LLVMValueRef func = LLVM.LLVMGetFirstFunction(module); func != null; func = LLVM.LLVMGetNextFunction(func)) {
             for (LLVMBasicBlockRef bb = LLVM.LLVMGetFirstBasicBlock(func); bb != null && !bb.isNull(); bb = LLVM.LLVMGetNextBasicBlock(bb)) {
@@ -391,6 +391,6 @@ public class LLVMIROptimization {
             //System.out.println(LLVM.LLVMPrintValueToString(x).getString());
             LLVM.LLVMInstructionEraseFromParent(x);
         }
-        System.out.println("end");
+        //System.out.println("end");
     }
 }
