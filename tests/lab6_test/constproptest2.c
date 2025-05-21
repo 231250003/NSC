c3834ba76e6d9307475b7004e9595873b293ea83
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-21 13:54:01 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:54:01 up  5:24,  1 user,  load average: 1.32, 2.18, 2.46 8d8992155e4fc06fb8fca4575babe3ff5a5209d

--- tests/lab6_test/constproptest2.c
+++ tests/lab6_test/constproptest2.c
@@ -4,12 +4,12 @@ int main() {
     int num = 1;
     if(num==1) num=num+1;
     return num;
-    /*
+    
     int c = num;
     if(c==1) c=c+1;
     while(num<=10) {
         num=num+1;
      }
     return num * num + c;
-    */
+
 }
