23f71d3fcae3b17067d28fa877ec9a34d4cccfb8
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-21 13:13:44 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:13:44 up  4:43,  1 user,  load average: 5.64, 4.36, 2.35 e98f3dba7d61e4babb7d4699605da1ef118013e6

--- src/LLVMIROptimization.java
+++ src/LLVMIROptimization.java
@@ -380,9 +380,9 @@ public class LLVMIROptimization {
                 }
             }
         }
-//        for(LLVMValueRef x:inst_to_delete) {
-//            System.out.println(LLVM.LLVMPrintValueToString(x).getString());
-//            LLVM.LLVMInstructionEraseFromParent(x);
-//        }
+        for(LLVMValueRef x:inst_to_delete) {
+            System.out.println(LLVM.LLVMPrintValueToString(x).getString());
+           // LLVM.LLVMInstructionEraseFromParent(x);
+        }
     }
 }
