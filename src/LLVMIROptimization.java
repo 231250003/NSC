9dec0fb17f2a92394ce4734e406e7eeea9937150
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-21 13:08:19 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:08:19 up  4:37,  1 user,  load average: 1.12, 1.25, 0.92 9d32e8c890607bd9f6251b0d135f0e234a96139

--- src/LLVMIROptimization.java
+++ src/LLVMIROptimization.java
@@ -340,6 +340,7 @@ public class LLVMIROptimization {
                         }
                         else{
                             if(LLVM.LLVMIsConstant(LLVM.LLVMGetOperand(inst, 0))!=0&&is_constant.contains(LLVM.LLVMGetValueName(LLVM.LLVMGetOperand(inst, 0)).getString())){
+                                System.out.println("crczzzz");
                                 LLVMValueRef constInst = LLVMConstInt(LLVM.LLVMTypeOf(LLVM.LLVMGetOperand(inst, 0)), out_inst.get(inst).get(LLVM.LLVMGetValueName(LLVM.LLVMGetOperand(inst, 0)).getString()).getIntValue(), 0);
                                 LLVM.LLVMSetOperand(inst, 0, constInst);
                             }
