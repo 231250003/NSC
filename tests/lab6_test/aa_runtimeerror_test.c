2661e43a069a99d4779b7153253743562d3f7038
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-21 23:58:20 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:58:20 up  5:21,  1 user,  load average: 2.86, 2.01, 1.00 d9104db9cb536ac8f190817ba81d233cf3d323

--- tests/lab6_test/aa_runtimeerror_test.c
+++ tests/lab6_test/aa_runtimeerror_test.c
@@ -9,11 +9,14 @@ int main() {
     int g = 0;      // REM (0)
     // 循环控制测试
     int sum = 0;
-        
+        int n = 0;
+         while (n < 5) {
+              sum = sum + n;
+              n = n + 1;
+            }
 
      int mem_test = 0;
      mem_test = c;
-     int load_test = mem_test;
 
     // 复合条件验证
     return (c - 18)    // 验证加法
