abd28470aafb1fb7f3091572c681ed1d564e9173
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-21 23:16:08 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:16:08 up  4:40,  1 user,  load average: 0.20, 0.37, 0.53 3d6bbfb34b5b996978e0ee0fdad5e0885b39f20d

---
+++ tests/lab6_test/aa_runtimeerror_test.c
@@ -0,0 +1,18 @@
+int x=3;
+int main(){
+     int num=0;
+     if(num==1) return num;
+     x=x+1;
+     int c=2;
+     c=num;
+     c=c+1;
+     while(num<=10){
+        while(x<=4){
+            x=x+1;
+            break;
+        }
+        num=num+1;
+        c=c+1;
+     }
+     return c+num;
+}
