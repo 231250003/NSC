2066f5118dc966b31fb599a61dbf2075927b5bb3
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-21 20:44:36 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:44:36 up  2:27,  1 user,  load average: 2.77, 1.67, 1.16 50676f69e8ff9d856867020384cd73ca78a908fe

--- src/LLVMIROptimization.java
+++ src/LLVMIROptimization.java
@@ -506,6 +506,7 @@ public class LLVMIROptimization {
         return flag;
     }
     public void elem_dead_code(){
+        System.out.println("crzzz");
         buildgraph();
         for (LLVMValueRef func = LLVM.LLVMGetFirstFunction(module); func != null && !func.isNull(); func = LLVM.LLVMGetNextFunction(func)) {
             for (LLVMBasicBlockRef bb = LLVM.LLVMGetFirstBasicBlock(func); bb != null && !bb.isNull(); bb = LLVM.LLVMGetNextBasicBlock(bb)) {
