33efa46772718939c773fca420e08655cd06960b
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-21 22:12:44 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:12:44 up  3:55,  1 user,  load average: 1.36, 1.08, 0.83 b4935581e54026f8484694894f8b53de04787f3

--- src/LLVMIROptimization.java
+++ src/LLVMIROptimization.java
@@ -512,14 +512,14 @@ public class LLVMIROptimization {
             LLVMInsertIntoBuilder(builder, inst);
         }
         LLVMRemoveBasicBlockFromParent(src);
-        for(Map.Entry<LLVMValueRef,Set<LLVMValueRef>> entry:predecessor.entrySet()){
+        for(Map.Entry<LLVMValueRef,Set<LLVMValueRef>> entry:successor.entrySet()){
             System.out.println(LLVMPrintValueToString(entry.getKey()).getString());
             for(LLVMValueRef tem:(entry.getValue())){
                 System.out.println(LLVMPrintValueToString(tem).getString());
             }
             System.out.println("-------------");
         }
-        System.out.println("end of predecessor check");
+        System.out.println("end of successor check");
     }
     public boolean merge_block(){
        for(Map.Entry<LLVMValueRef,Set<LLVMValueRef>> entry:successor.entrySet()){
