cc81ff790f0df694368266e4a45f6cd48e9e1475
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-21 14:54:59 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:54:59 up  6:27,  1 user,  load average: 0.43, 0.60, 0.75 1b231c62487442f404e486a9f3df176f210040c

--- src/LLVMIROptimization.java
+++ src/LLVMIROptimization.java
@@ -339,8 +339,8 @@ public class LLVMIROptimization {
                         if(is_constant.contains(dest)) {
                             if (!has_store_variable.contains(dest)) {
                                 has_store_variable.add(dest);
-                                System.out.println( out_inst.get(inst).get(dest));
-                                System.out.println(LLVMPrintValueToString(inst).getString());
+//                                System.out.println( out_inst.get(inst).get(dest));
+//                                System.out.println(LLVMPrintValueToString(inst).getString());
                                 LLVMValueRef constInst = LLVMConstInt(LLVM.LLVMTypeOf(LLVM.LLVMGetOperand(inst, 0)), out_inst.get(inst).get(dest).getIntValue(), 0);
                                 LLVM.LLVMSetOperand(inst, 0, constInst);
                             }
