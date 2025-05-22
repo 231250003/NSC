66447347275b4e2fe9756bb54992099065e56b66
231250003-Chen rui ze <crz@DESKTOP-LJJBL1C>
2025-05-22 17:48:35 +0800

    crz

--- src/LLVMIROptimization.java
+++ src/LLVMIROptimization.java
@@ -613,7 +613,7 @@ public class LLVMIROptimization {
         }
         cleanUnreachableBlocks();
         buildgraph();
-        if(IRGenerationVisitor.while_stmt_count>=1){
+        if(IRGenerationVisitor.while_stmt_count>=2){
             while(simplifySingleInstructionBlocks());
         }
         while(remove_redundant_block());
