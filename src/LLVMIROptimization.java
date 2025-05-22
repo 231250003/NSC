61275cf354c50fea65b1f39fb1a6649f3b5080b3
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-22 17:56:48 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:56:48 up  9:20,  1 user,  load average: 0.09, 0.34, 0.34 a7262257c8859f9416a9c26205578596a1eeca2

--- src/LLVMIROptimization.java
+++ src/LLVMIROptimization.java
@@ -613,7 +613,7 @@ public class LLVMIROptimization {
         }
         cleanUnreachableBlocks();
         buildgraph();
-        if(IRGenerationVisitor.if_stmt_count>=1){
+        if(IRGenerationVisitor.if_stmt_count>=0){
             while(simplifySingleInstructionBlocks());
         }
         while(remove_redundant_block());
