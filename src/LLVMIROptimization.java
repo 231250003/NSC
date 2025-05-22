b4034ec0c2493706d1e506282ce25ff1c7c74a24
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-22 09:40:48 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:40:48 up  1:33,  1 user,  load average: 0.44, 0.49, 0.41 d301e99b39c05f99830827f5c1ba5778f32daf27

--- src/LLVMIROptimization.java
+++ src/LLVMIROptimization.java
@@ -299,6 +299,7 @@ public class LLVMIROptimization {
             System.out.println("--------------");
             out_inst.put(inst, new_out);
         }
+        System.out.println("end of const prop");
         Set<String> is_constant=new HashSet<>();
         for(Map.Entry<String,ConstPropValueHolder> entry:constpropinit.entrySet()){
             is_constant.add(entry.getKey());
