6d09174bffb929418b50537f2aadc7ebe4a2f62f
231250003-Chen rui ze <crz@DESKTOP-LJJBL1C>
2025-05-22 16:22:39 +0800

    crz

--- src/LLVMIROptimization.java
+++ src/LLVMIROptimization.java
@@ -614,7 +614,7 @@ public class LLVMIROptimization {
         }
         cleanUnreachableBlocks();
         buildgraph();
-        if(Main.radical_optimize>0.5)while(simplifySingleInstructionBlocks());
+        if(Main.radical_optimize>0.35)while(simplifySingleInstructionBlocks());
         while(remove_redundant_block());
         return ret;
     }
