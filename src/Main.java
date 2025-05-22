66c341b5ebcd780cba0baf90fed25de8def75a7f
231250003-Chen rui ze <crz@DESKTOP-LJJBL1C>
2025-05-22 16:38:56 +0800

    crz

--- src/Main.java
+++ src/Main.java
@@ -84,7 +84,7 @@ public class Main {
         }
         var_num=get_var_num(module);
         LLVMIROptimization optimization=new LLVMIROptimization(module);
-        //radical_optimize=Math.random();
+        radical_optimize=Math.random();
         clean_terminator_inst(module);
         boolean flag1=true,flag2=true,flag3=true;
         while(flag1||flag2||flag3) {
