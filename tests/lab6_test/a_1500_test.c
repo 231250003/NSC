7e87e423230f61d32437c02c0e7e9896a27a748f
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-22 17:58:11 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:58:11 up  9:22,  1 user,  load average: 0.11, 0.29, 0.32 b8d69c6e86ce8f11d1333a9efe8ef17a9fc643ac

--- tests/lab6_test/a_1500_test.c
+++ tests/lab6_test/a_1500_test.c
@@ -1,5 +1,12 @@
 int main() {
-   int num=0;
-   while(num<=10) num=num+1;
-   return num;
+    int n, i, sum;
+    n   = 20;    // 读入 n
+    i   = 1;
+    sum = 0;
+    while (i <= n) {   // 唯一的 while 循环
+        sum = sum + i;
+        i   = i + 1;
+    }
+    putint(sum);       // 输出结果
+    return 0;
 }
