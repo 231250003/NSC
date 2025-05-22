c56dc87e8a1d7426e259e834976ca5d46f26c03a
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-22 10:38:32 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:38:32 up  2:29,  1 user,  load average: 0.28, 0.74, 0.59 aa50550be63c3b8f98fe9bd2d161b85ddd414b32

--- src/Main.java
+++ src/Main.java
@@ -83,7 +83,7 @@ public class Main {
         var_num=get_var_num(module);
         LLVMIROptimization optimization=new LLVMIROptimization(module);
         boolean flag1=true,flag2=true,flag3=true;
-       for(int i=1;i<=1000;i++) {
+        while(flag1||flag2||flag3) {
             flag1=optimization.constprop();
             flag2=optimization.elem_unused();
             flag3=optimization.elem_dead_code();
