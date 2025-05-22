6787852afc0edd16729d946ee1cda991078b7fdc
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-22 16:29:36 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:29:36 up  7:55,  1 user,  load average: 0.11, 0.17, 0.40 d321ae3b19ea5181dc3abc98fabe9c3b94a0df4

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
     public static boolean is_run_time_error_test=false;
     public static boolean used_interpreter=false;
     public static int var_num;
-    public static double radical_optimize;
+    public static double radical_optimize=1;
     public static void main(String[] args) throws IOException {
         /* if (args.length < 1) {
             System.err.println("input path is required");
@@ -83,7 +83,7 @@ public class Main {
         }
         var_num=get_var_num(module);
         LLVMIROptimization optimization=new LLVMIROptimization(module);
-        radical_optimize=Math.random();
+        //radical_optimize=Math.random();
         boolean flag1=true,flag2=true,flag3=true;
         while(flag1||flag2||flag3) {
             flag1=optimization.constprop();
