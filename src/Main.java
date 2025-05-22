81c378299e5260557dfc8bf3bcad6fd6579323a0
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-05-22 18:23:02 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:23:02 up  9:46,  1 user,  load average: 2.02, 2.04, 1.20 8be12f2784e500869d736e753b7cf982d337d33

--- src/Main.java
+++ src/Main.java
@@ -83,13 +83,13 @@ public class Main {
         }
         var_num=get_var_num(module);
         LLVMIROptimization optimization=new LLVMIROptimization(module);
-        //clean_terminator_inst(module);
-//        boolean flag1=true,flag2=true,flag3=true;
-//        while(flag1||flag2||flag3) {
-//            flag1=optimization.constprop();
-//            flag2=optimization.elem_unused();
-//            flag3=optimization.elem_dead_code();
-//        }
+        clean_terminator_inst(module);
+        boolean flag1=true,flag2=true,flag3=true;
+        while(flag1||flag2||flag3) {
+            flag1=optimization.constprop();
+            flag2=optimization.elem_unused();
+            flag3=optimization.elem_dead_code();
+        }
         BytePointer error = new BytePointer((Pointer) null);
         if (LLVMPrintModuleToFile(module, args[1], error) != 0) {
             LLVMDisposeMessage(error);
