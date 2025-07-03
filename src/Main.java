7a25191e08fc301889b27052d066072b6f8d8bde
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-03 15:37:09 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:37:09 up  6:23,  1 user,  load average: 0.37, 0.36, 0.34 f5581e282f42f2a04200c03e6b35862be0cd38cc

--- src/Main.java
+++ src/Main.java
@@ -76,8 +76,8 @@ public class Main {
         if (LLVMPrintModuleToFile(module, args[1], error) != 0) {
             LLVMDisposeMessage(error);
         }
-        LLVMIRToRiscv llvmirToRiscv=new LLVMIRToRiscv(module,args[1].substring(0,args[1].length()-3)+".riscv");
-        llvmirToRiscv.to_riscv();
+//        LLVMIRToRiscv llvmirToRiscv=new LLVMIRToRiscv(module,args[1].substring(0,args[1].length()-3)+".riscv");
+//        llvmirToRiscv.to_riscv();
         LLVMDisposeBuilder(builder);
         LLVMDisposeModule(module);
     }
