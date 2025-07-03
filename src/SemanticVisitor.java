1827cda731debeec529a32cd526de2708c2fe950
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-03 11:31:34 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:31:34 up  2:46,  1 user,  load average: 0.70, 1.20, 1.13 ff2bfe42c7391e6fa5c51991876af673cc5e98ab

--- src/SemanticVisitor.java
+++ src/SemanticVisitor.java
@@ -45,6 +45,8 @@ public class SemanticVisitor extends SysYParserBaseVisitor<Void> {
         }
         else params=new ArrayList<>();
         FunctionType functionType = new FunctionType(retType, params);
+        System.out.println("rzz");
+        System.out.println(params.get(1).type);
         symbolTable.addGlobal(new Symbol(funcName,functionType));
         visit_block(ctx.block(),params);
         return null;
