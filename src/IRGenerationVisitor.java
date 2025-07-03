d1781ab0db7b31a29c2b122d77a4d56d635b6756
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-04 00:37:44 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:37:44 up 15:10,  1 user,  load average: 1.32, 1.83, 1.63 82175e1189e4fe7a9815dd98dd680ffa0294204

--- src/IRGenerationVisitor.java
+++ src/IRGenerationVisitor.java
@@ -334,6 +334,7 @@ public class IRGenerationVisitor extends   SysYParserBaseVisitor<LLVMValueRef> {
             System.out.println("crzzz11");
             if (LLVMGetTypeKind(LLVMTypeOf(value)) ==LLVMPointerTypeKind&&pointer_need_load) {
                 System.out.println("crzzz12");
+                if(LLVMBuildLoad(builder, value, "load_lval")==null) System.out.println("2213432");
                 return castToI32(LLVMBuildLoad(builder, value, "load_lval"));
             } else {
                 return castToI32(value);
@@ -486,8 +487,6 @@ public class IRGenerationVisitor extends   SysYParserBaseVisitor<LLVMValueRef> {
     public LLVMValueRef visitCond(SysYParser.CondContext ctx) {
         if(ctx.exp()!=null){
             System.out.println("crzzzzzzzzz");
-            visit(ctx.exp());
-            System.out.println("crzzz565656");
             LLVMValueRef value = visit(ctx.exp());
             System.out.println("crzzz4");
             if(LLVMGetTypeKind(LLVMTypeOf(value))==LLVMPointerTypeKind) System.out.println("crzzz6");
