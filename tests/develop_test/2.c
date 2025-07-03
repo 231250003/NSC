e5d98199a58ef7a7b172bfc174297cf77fdef805
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-03 21:58:06 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:58:06 up 12:49,  1 user,  load average: 1.10, 1.08, 0.72 1a3333d3d8c255182abeca30471faa7426d3c457

--- tests/develop_test/2.c
+++ tests/develop_test/2.c
@@ -5,7 +5,7 @@ int combine(int arr1[2], int arr1_length, int arr2[3], int arr2_length) {
     int k = 0;
     while (i < arr1_length && j < arr2_length) {
         if (arr1[i] < arr2[j]) {
-            sort_arr[k] = arr1[i];
+            //sort_arr[k] = arr1[i];
             i = i + 1;
         }
         else {
