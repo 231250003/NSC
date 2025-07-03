f4677d3655f8d99e54a7a7517d4404cb405b472b
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-03 15:54:18 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:54:18 up  6:42,  1 user,  load average: 1.28, 2.31, 1.40 b511db6ebf1ac782748f560ab5239b8952b579c9

--- test_error_detection.sh
+++ test_error_detection.sh
@@ -2,6 +2,7 @@
 
 GENERATE_RISCV=0
 SYSY_DIR="tests/develop_test"
+ll_file="$SYSY_DIR/$filename.ll"
 echo "== Batch processing all .sysy files in $SYSY_DIR =="
 for sysy_file in "$SYSY_DIR"/*.sysy; do
     make -s run SRCFILE="$sysy_file" OUTFILE="$ll_file"
