6063ad49f951ac70acbb3048a5ca034b055b245a
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-03 20:20:45 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:20:45 up 11:11,  1 user,  load average: 0.33, 0.12, 0.16 80427382fe8363dada195fc5364061bd50900d6a

--- tests/develop_test/1.c
+++ tests/develop_test/1.c
@@ -1,12 +1,11 @@
-int sort_arr[5];
-int combine(int arr1[], int arr1_length, int arr2[], int arr2_length) {
-    int i = 0;
-    int j = 0;
-    return 0;
+int z=3;
+int print(){
+    return z+3;
 }
-
-int main() {
-    int a[2] = { 1,5 };
-    int b[3] = { 1,4,14 };
-    return combine(a, 2, b, 3);
+int main(){
+    int a[4][4]={{1,2,3},{3,4}};
+    int x=1;
+    //int t=print();
+    //int arg[200];
+    return 0;
 }
