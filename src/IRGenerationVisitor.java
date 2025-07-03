b64c1cfe5acd7496b3d3b758aa5e0a019b7707e7
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-03 11:22:02 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:22:02 up  2:35,  1 user,  load average: 0.65, 0.68, 0.65 11e9d9604e3f737eb5c4d5d7cde8a5f27eb761ab

--- src/IRGenerationVisitor.java
+++ src/IRGenerationVisitor.java
@@ -300,7 +300,7 @@ public class IRGenerationVisitor extends   SysYParserBaseVisitor<LLVMValueRef> {
         Type type;
         String name=ctx.IDENT().getText();
         if (ctx.L_BRACKT().size() > 0) {
-            type=new ArrayType(new IntType(),1);
+            type=new ArrayType(new IntType(),ctx.L_BRACKT().size());
         }
         else type=new IntType();
         return new Symbol(name,type);
