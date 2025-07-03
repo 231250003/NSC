f4e1a040ec6650fe6d88bd97c4057d23189619fb
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-03 14:09:38 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:09:38 up  4:49,  1 user,  load average: 0.25, 0.47, 0.40 b14a429e5df6d0efd2f97f45cd9fdfd2546011b

--- src/Main.java
+++ src/Main.java
@@ -40,6 +40,7 @@ public class Main {
          SysYParser parser = new SysYParser(tokens);
          parser.removeErrorListeners();
         MyErrorListener parser_errorListener=new MyErrorListener(errorType.SYNTAX_ERROR);
+        parser.addErrorListener(parser_errorListener);
         ParseTree tree = parser.program();  // 或 parser.compUnit()
         if(parser_errorListener.hasErrorInformation()){
             parser_errorListener.printErrorInformation();
