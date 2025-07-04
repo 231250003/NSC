126160552c7e73d6b4b52628ee5a56329f0695b2
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-04 16:03:57 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:03:57 up 4 min,  1 user,  load average: 0.89, 0.56, 0.25 8322b3d18e6196cf97969caab4fee048d229f0

--- tests/develop_test/3.c
+++ tests/develop_test/3.c
@@ -5,14 +5,14 @@ int max(int a,int b){
 int main(){
     int w[1001]={2,3,4,7},c[1001]={1,3,5,9},f[101][1001];
 	int n=10,m=4;
-	int i=1,j=1;
+	int i=1,v=1;
     while(i<=n)
     {
-    	while(j<=m)
+    	while(v<=m)
     	{
     		if(v>=w[i]) f[i][v]=max(f[i-1][v-w[i]]+c[i],f[i-1][v]);
     		else f[i][v]=f[i-1][v];
-    		j++;
+    		v++;
 		}
 		i++;
 	}
