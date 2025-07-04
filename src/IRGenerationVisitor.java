cf074c1b49e4c13f0685245e7cc6c46b31e11b32
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-04 09:58:46 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:58:46 up 57 min,  1 user,  load average: 0.63, 0.65, 0.83 799e73cb3eb500c6b5ab51d5e35a45ddd0b47aad

--- src/IRGenerationVisitor.java
+++ src/IRGenerationVisitor.java
@@ -78,7 +78,7 @@ public class IRGenerationVisitor extends   SysYParserBaseVisitor<LLVMValueRef> {
             for (int i = 0; i < paramCount; i++) {
                 LLVMValueRef param = LLVMGetParam(function, i);
                 LLVMTypeRef paramType = LLVMTypeOf(param);
-                if(LLVMGetTypeKind(LLVMTypeOf(param))==LLVMPointerTypeKind) System.out.println("this is it");
+                //if(LLVMGetTypeKind(LLVMTypeOf(param))==LLVMPointerTypeKind) System.out.println("this is it");
                 LLVMValueRef paramAddr = LLVMBuildAlloca(builder, paramType, "param" + i + "_addr");
                 LLVMBuildStore(builder, param, paramAddr);
                 Symbol symbol = symbols.get(i);
