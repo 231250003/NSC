d657503c32396a71061941ac2a7a71401a62fdb5
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-07 14:27:35 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:27:35 up  5:51,  1 user,  load average: 0.13, 0.19, 0.29 63cf66568dd2d63e79441b6696bc0234e91b44d8

--- src/SemanticVisitor.java
+++ src/SemanticVisitor.java
@@ -5,7 +5,6 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 
 import java.util.*;
 
-import semantic_check.*;
 public class SemanticVisitor extends SysYParserBaseVisitor<Void> {
     private SymbolTable symbolTable=new SymbolTable();
     public Void visitProgram(SysYParser.ProgramContext ctx) {
