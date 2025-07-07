818813172537a623c3789b1bf2c32219bb9c776b
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-07 23:01:35 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:01:35 up 25 min,  1 user,  load average: 0.47, 0.29, 0.21 114858f5543485d316be6301391c2e2643134fad

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ import org.bytedeco.llvm.global.LLVM;
 import static org.bytedeco.llvm.global.LLVM.*;
 
 public class Main {
-    public static boolean is_run_time_error_test=false;
+    public static boolean is_run_time_error_test=true;
     //public static boolean used_interpreter=false;
     public static void main(String[] args) throws IOException {
         /* if (args.length < 1) {
@@ -75,8 +75,8 @@ public class Main {
         if (LLVMPrintModuleToFile(module, args[1], error) != 0) {
             LLVMDisposeMessage(error);
         }
-//        LLVMIRToRiscv llvmirToRiscv=new LLVMIRToRiscv(module,args[1].substring(0,args[1].length()-3)+".riscv");
-//        llvmirToRiscv.to_riscv();
+        LLVMIRToRiscv llvmirToRiscv=new LLVMIRToRiscv(module,args[1].substring(0,args[1].length()-3)+".riscv");
+        llvmirToRiscv.to_riscv();
         LLVMDisposeBuilder(builder);
         LLVMDisposeModule(module);
     }
