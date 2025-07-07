48a076ebb3b7088fbec8e95c4984ef2cdea8c619
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-07 11:13:05 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:13:05 up  2:20,  1 user,  load average: 0.74, 0.69, 0.45 7dc59c8d7e98e44d868da19f180b5e2a42c9916

---
+++ tests/develop_test/while_test.c
@@ -0,0 +1,33 @@
+int e[101][101],dis[1001],book[1001];
+int main()
+{
+	    int x=1;
+	    int y=2;
+	    while(1)
+	    {
+	        while(x<10){
+	            x=x+1;
+	            {
+	                {
+	                    {
+	                        {
+
+	                            x=y;
+	                        }
+	                    }
+	                }
+	                    while(y<20){
+	                        y=x+y;
+
+	                    }
+
+	            }
+	            x=x+1;
+	        }
+	        x=x+2;
+	        {
+	            {if(x>30) break;}
+	        }
+	    }
+	    return x+y;
+}
