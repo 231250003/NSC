bde1c3042550d47df888c45a53a1abb58138658a
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-11 13:46:30 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:46:30 up  5:07,  1 user,  load average: 1.03, 0.46, 0.42 be03a5d924871c4bd485ae185f2d3a06ef410bbb

--- tests/develop_test/regalloctestcomp.c
+++ tests/develop_test/regalloctestcomp.c
@@ -17,8 +17,6 @@ int main() {
     // Perform some operations on local variables
     x1 = x1 * 2;
     x2 = x2 * 2;
-    x3 = x3 * 2;
-    x4 = x4 * 2;
 
     return x1 + x2 + x3 + x4 +
     + x + y + z ;
