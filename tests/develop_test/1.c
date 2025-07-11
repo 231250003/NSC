975133e04633bbb79c62cc5a2e7f4b29a53e439d
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-11 21:55:11 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:55:11 up  3:42,  1 user,  load average: 0.41, 0.60, 0.44 93470edfdcfe9d38535e8b4a62fd2b6f36fbc5b9

--- tests/develop_test/1.c
+++ tests/develop_test/1.c
@@ -1,10 +1,10 @@
 int z=3;
 int main(){
-    int a[4]={1,2,3};
+    int a[4][4]={{1,2,3},{3,4}};
     int x=1;
     int sum=0;
     for(int i=0;i<3;i=i+1){
-        sum=sum+a[i];
+        sum=sum+a[0][i];
     }
     return sum;
 }
