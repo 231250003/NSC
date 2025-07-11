b7df9c98ab224953899374807f445a1eeefdffc2
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-11 13:32:14 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:32:14 up  4:52,  1 user,  load average: 1.07, 0.70, 0.52 ea9e0c9ae3f33d9a79e1c2dab341adc6808f7cd1

--- tests/develop_test/controltest2.c
+++ tests/develop_test/controltest2.c
@@ -4,7 +4,6 @@ int y = 98;
 int main() {
     int a = x;
     int b = y;
-    /*
      if(a%2==1) a=a+1;
     while (b != 0) {
         if (a > b) {
@@ -13,7 +12,6 @@ int main() {
             b = b - a;
         }
     }
-    */
     int result = a;
 	return result;
 }
