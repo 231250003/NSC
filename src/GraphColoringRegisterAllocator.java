874fd3831837ba69da199aa432c12ae93381ece2
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-11 10:35:45 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:35:45 up  1:37,  1 user,  load average: 6.35, 2.51, 1.21 c63aafcc38f64b875997adab039d86dced1699e

--- src/GraphColoringRegisterAllocator.java
+++ src/GraphColoringRegisterAllocator.java
@@ -240,10 +240,10 @@ public class GraphColoringRegisterAllocator implements RegisterAllocator {
         create_graph(func);
         color_graph();
         for (LLVMBasicBlockRef bb = LLVM.LLVMGetFirstBasicBlock(func); bb != null && !bb.isNull(); bb = LLVM.LLVMGetNextBasicBlock(bb)){
+            System.out.println();
+            System.out.println();
+            System.out.println();
             System.out.println(LLVM.LLVMGetBasicBlockName(bb).getString());
-            System.out.println();
-            System.out.println();
-            System.out.println();
             for (LLVMValueRef inst = LLVM.LLVMGetFirstInstruction(bb); inst != null; inst = LLVM.LLVMGetNextInstruction(inst)){
                 System.out.println(LLVM.LLVMPrintValueToString(inst).getString());
                 for(String x:in_inst.get(inst)){
