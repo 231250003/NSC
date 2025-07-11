cfd6d83ddb6776c7867fa27cc6e449c1eabaf9a9
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-11 15:08:07 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:08:07 up  6:37,  1 user,  load average: 0.50, 0.59, 0.47 2eb885bf7ce046d5cda843eddc95aa4b8f30e3ea

--- src/RegisterAllocator.java
+++ src/RegisterAllocator.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public interface RegisterAllocator {
-    //List<String> available_register= Arrays.asList("x8","x9","x11");
-    List<String> available_register=new ArrayList<>();
+    List<String> available_register= Arrays.asList("x8","x9","x11","x12","x13","x14","x15","x16");
     String allocate(String varName);
 }
