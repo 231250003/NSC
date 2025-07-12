f7f0e4e73ee5aeb41a37f19212f39d217a7ecad1
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-12 14:11:52 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:11:52 up  5:49,  1 user,  load average: 2.70, 1.22, 0.66 72c3bd44cb625c91f0b8c5ed64d9b64425614454

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -85,9 +85,9 @@ public class LLVMIRToRiscv {
             asm.label(funcName);
             if ("main".equals(funcName)) asm.instr("addi", "sp", "sp", "-" + 2044);
             allocator = new GraphColoringRegisterAllocator(func);
+            System.out.println("crzzzzz");
             next_offset = 0;
             int paramCount = LLVM.LLVMCountParams(func);
-            System.out.println("crzzzzz");
             for (int i = 8; i < paramCount; i++) {
                 LLVMValueRef param = LLVM.LLVMGetParam(func, i);
                 String paramName = LLVM.LLVMGetValueName(param).getString();
