a0d467ddc757fdce088a237b0a56e296998c9349
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-12 21:58:41 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:58:41 up 14:15,  1 user,  load average: 0.34, 0.34, 0.53 2d67159c968ba2a01fdd179a8f2822e9593e7404

--- tests/develop_test/quicksort.c
+++ tests/develop_test/quicksort.c
@@ -9,7 +9,7 @@ void swap(int arr[6], int low, int high) {
 int main() {
     int arr[6] = {10, 7, 8, 9, 1, 5};
     int n = 6;
-    swap(arr, 1,n-1);
+    swap(arr, 1,5);
     return arr[1];
     //return 0;
 }
