921c5e26d0376167dd80d865bd1b3687fdb358db
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-12 19:46:11 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:46:11 up 11:51,  1 user,  load average: 0.53, 0.42, 0.25 5d62f6965b7fad1e343a1dc41906feb12f0d0c8c

--- src/GraphColoringRegisterAllocator.java
+++ src/GraphColoringRegisterAllocator.java
@@ -143,7 +143,7 @@ public class GraphColoringRegisterAllocator implements RegisterAllocator {
                     }
                     for (array_variable x : array_variable_ref) {
                         if (naive_alias_may_analysis(x, cur) == true) {
-                            System.out.println(cur.variable_name);
+                            System.out.println(x.variable_name);
                             variable_use_in_block.get(LLVM.LLVMGetBasicBlockName(bb).getString()).add(x.variable_name);
                         }
                     }
