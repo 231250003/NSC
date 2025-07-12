b20dd27714383d88df379a23675085624d3a0091
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-12 21:41:56 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:41:56 up 13:57,  1 user,  load average: 0.63, 1.00, 0.70 5e19547e7c853ce5d96aacdc96ce24d4de114eb

--- tests/develop_test/quicksort.c
+++ tests/develop_test/quicksort.c
@@ -7,13 +7,31 @@ void swap(int arr[6], int i, int j) {
 
 // 分区函数
 int partition(int arr[6], int low, int high) {
+    //int pivot = arr[high];  // 选择最后一个元素作为基准
+
+    /*for (int j = low; j < high; j =j+ 1) {
+        // 如果当前元素小于或等于基准
+        if (arr[j] <= pivot) {
+            i = i+1;         // 扩大小于基准的区域
+            swap(arr, i, j); // 将当前元素放到该区域内
+        }
+    }*/
+
+    // 将基准放到正确位置
     swap(arr, low, high);
-    return low;
+    return low;           // 返回基准的最终位置
 }
 
 // 快速排序主函数
 void quickSort(int arr[6], int low, int high) {
-     partition(arr, low, high);
+    if (low < high) {
+        // 找到分区点
+        int pi = partition(arr, low, high);
+
+        // 递归排序分区
+        //quickSort(arr, low, pi - 1);
+        //quickSort(arr, pi + 1, high);
+    }
 }
 
 // 打印数组
@@ -21,6 +39,7 @@ void quickSort(int arr[6], int low, int high) {
 int main() {
     int arr[6] = {10, 7, 8, 9, 1, 5};
     int n = 6;
-    quickSort(arr, 1, n-1);
+    quickSort(arr, 1, n - 1);
     return arr[0];
+    //return 0;
 }
