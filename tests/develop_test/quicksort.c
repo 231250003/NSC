3e864cca7cae3fdbc9bf4d4c1593f1a0c89998ad
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-12 21:06:46 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:06:46 up 13:19,  1 user,  load average: 1.02, 0.69, 0.46 2779aac8c400a03b2351dc7452d778bcdb2f053a

--- tests/develop_test/quicksort.c
+++ tests/develop_test/quicksort.c
@@ -13,7 +13,7 @@ int partition(int arr[6], int low, int high) {
 
 // 快速排序主函数
 void quickSort(int arr[6], int low, int high) {
-        int pi = partition(arr, low, high);
+       partition(arr, low, high);
 }
 
 // 打印数组
