255a63ee62d8888ed62d63dd68cd1a5d2cce99ad
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-12 12:52:08 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:52:08 up  4:23,  1 user,  load average: 1.06, 0.74, 0.46 f910723858eb5104239ad40b51e2fd8d28f2637

--- src/GraphColoringRegisterAllocator.java
+++ src/GraphColoringRegisterAllocator.java
@@ -322,20 +322,20 @@ public class GraphColoringRegisterAllocator implements RegisterAllocator {
         cal_in_out_block(func);
         create_graph(func);
         color_graph();
-        for (LLVMBasicBlockRef bb = LLVM.LLVMGetFirstBasicBlock(func); bb != null && !bb.isNull(); bb = LLVM.LLVMGetNextBasicBlock(bb)){
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println(LLVM.LLVMGetBasicBlockName(bb).getString());
-            for (LLVMValueRef inst = LLVM.LLVMGetFirstInstruction(bb); inst != null; inst = LLVM.LLVMGetNextInstruction(inst)){
-                System.out.println(LLVM.LLVMPrintValueToString(inst).getString());
-                for(String x:out_inst.get(inst)){
-                    System.out.print(x);
-                    System.out.print(" ");
-                }
-                System.out.println();
-            }
-        }
+//        for (LLVMBasicBlockRef bb = LLVM.LLVMGetFirstBasicBlock(func); bb != null && !bb.isNull(); bb = LLVM.LLVMGetNextBasicBlock(bb)){
+//            System.out.println();
+//            System.out.println();
+//            System.out.println();
+//            System.out.println(LLVM.LLVMGetBasicBlockName(bb).getString());
+//            for (LLVMValueRef inst = LLVM.LLVMGetFirstInstruction(bb); inst != null; inst = LLVM.LLVMGetNextInstruction(inst)){
+//                System.out.println(LLVM.LLVMPrintValueToString(inst).getString());
+//                for(String x:out_inst.get(inst)){
+//                    System.out.print(x);
+//                    System.out.print(" ");
+//                }
+//                System.out.println();
+//            }
+//        }
 //        for (LLVMBasicBlockRef bb = LLVM.LLVMGetFirstBasicBlock(func); bb != null && !bb.isNull(); bb = LLVM.LLVMGetNextBasicBlock(bb)){
 //            System.out.println();
 //            System.out.println(LLVM.LLVMGetBasicBlockName(bb).getString());
@@ -353,11 +353,11 @@ public class GraphColoringRegisterAllocator implements RegisterAllocator {
 //            System.out.println();
 //
 //        }
-        for (Map.Entry<String, String> entry : valueMap.entrySet()) {
-            System.out.print(entry.getKey());
-            System.out.print(" ");
-            System.out.println(entry.getValue());
-        }
+//        for (Map.Entry<String, String> entry : valueMap.entrySet()) {
+//            System.out.print(entry.getKey());
+//            System.out.print(" ");
+//            System.out.println(entry.getValue());
+//        }
     }
 
     public String allocate(String varName) {
