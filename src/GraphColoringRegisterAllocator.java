548f1c871575df27b25694d6a7327a8da4640bca
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-12 19:45:19 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:45:19 up 11:50,  1 user,  load average: 0.38, 0.37, 0.22 246753ced72ba64acc8cbb62ce59e7ea93a4299c

--- src/GraphColoringRegisterAllocator.java
+++ src/GraphColoringRegisterAllocator.java
@@ -164,7 +164,7 @@ public class GraphColoringRegisterAllocator implements RegisterAllocator {
 
     public static boolean naive_alias_may_analysis(array_variable dest, array_variable src) {
         if (!dest.array_name.equals(src.array_name)) return false;
-        if(dest.variable_name.equals(src.variable_name)) return false;
+        //if(dest.variable_name.equals(src.variable_name)) return false;
         for (int i = 0; i < Math.min(dest.cur_offset.size(), src.cur_offset.size()); i++){
             if (dest.cur_offset.get(i) instanceof Integer && src.cur_offset.get(i) instanceof Integer&&(!src.cur_offset.get(i).equals(dest.cur_offset.get(i)))){
                 return false;
