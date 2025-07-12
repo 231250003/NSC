e67ae89509025606d8bae385dda7e8902fa46c69
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-13 01:04:22 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:04:22 up 16:59,  1 user,  load average: 1.86, 0.78, 0.54 53b3891a9133c1ca250e9afea002695cefa9577b

--- tests/develop_test/quicksort.c
+++ tests/develop_test/quicksort.c
@@ -17,6 +17,6 @@ void quickSort(int arr[6], int low, int high) {
 int main() {
     int arr[6] = {10, 7, 8, 9, 1, 5};
     int n = 6;
-    quickSort(arr, 1, n - 1);
+    quickSort(arr, 0, n - 1);
     return arr[5];
 }
