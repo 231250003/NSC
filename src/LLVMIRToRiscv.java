3e2647bc8267c4b605276ce8ca2cbc7fe3763197
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-12 17:09:13 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:09:13 up  9:01,  1 user,  load average: 1.26, 0.89, 1.00 8f1396ee6ce3042a16babd5dc06c9952cbe7c13a

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -15,7 +15,7 @@ public class LLVMIRToRiscv {
     RegisterAllocator allocator;
     int next_offset = 0;
     Map<String, String> value_stack_addr = new HashMap<>();
-    int phi_val_storage=1200;
+    int phi_val_storage=800;
     public LLVMIRToRiscv(LLVMModuleRef moduleRef, String file_path) {
         this.module = moduleRef;
         this.file_path = file_path;
