306f417dabe414451fbaa0cb8c1fce30f3ba0c52
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-13 16:38:54 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:38:54 up  4:45,  1 user,  load average: 0.15, 0.76, 0.87 4f46f199f6f9169170a2cf9c215e6e82a27aa477

--- tests/develop_test/quicksort.c
+++ tests/develop_test/quicksort.c
@@ -13,15 +13,13 @@ int partition(int arr[6], int low, int high) {
             swap(arr, i, j); // 将当前元素放到该区域内
         }
     }
+
+    // 将基准放到正确位置
     swap(arr, i + 1, high);
-    return i + 1;
+    return i + 1;           // 返回基准的最终位置
 }
 void quickSort(int arr[6], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-       // quickSort(arr, low, pi - 1);
-        //quickSort(arr, pi + 1, high);
-    }
+     int pi = partition(arr, low, high);
 }
 int main() {
     int arr[6] = {10, 7, 8, 9, 1, 5};
