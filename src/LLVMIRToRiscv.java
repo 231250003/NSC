b3693b0e79332fee82397780bc841cf17db7367f
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-13 17:44:28 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:44:28 up  5:53,  1 user,  load average: 1.57, 1.20, 0.68 de17c2565d2af64260c38aa8f7136df6b97285e

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -189,17 +189,17 @@ public class LLVMIRToRiscv {
                 asm.label(funcName + "_" + label);
                 List<array_variable> array_variable_ref = new ArrayList<>();//在函数调用中参数涉及函数时会用到
                 for (LLVMValueRef inst = LLVM.LLVMGetFirstInstruction(bb); inst != null && !inst.isNull(); inst = LLVM.LLVMGetNextInstruction(inst)) {
+                    if(funcName.equals("b")) {
+                        System.out.println(LLVM.LLVMPrintValueToString(inst).getString());
+                        if(value_stack_addr.get("elemPtr7")!=null) System.out.println( value_stack_addr.get("elemPtr7"));
+                        else System.out.println("elemPtr7 isn't set");
+                    }
                     //System.out.println(LLVMPrintValueToString(inst).getString());
                     int opcode = LLVM.LLVMGetInstructionOpcode(inst);
                     if (opcode == LLVM.LLVMGetElementPtr) {//注意getelementptr的下标可能是变量
                         boolean has_variable_index = false;
                         if (allocator.allocate(LLVM.LLVMGetValueName(inst).getString()) == null) continue;
                         String variable_name = LLVM.LLVMGetValueName(inst).getString();
-                        if(variable_name.equals("elemPtr7")&&funcName.equals("b")) {
-                            System.out.println("safdsadfs");
-                            System.out.println(LLVM.LLVMPrintValueToString(inst).getString());
-                            if(value_stack_addr.get(variable_name)!=null) System.out.println( value_stack_addr.get(variable_name));
-                        }
                         LLVMValueRef base_ptr = LLVM.LLVMGetOperand(inst, 0);
                         String array_name = LLVM.LLVMGetValueName(base_ptr).getString();
                         LLVMTypeRef base_type = LLVM.LLVMTypeOf(base_ptr);
