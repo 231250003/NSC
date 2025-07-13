c034ee3fea3ddb1d87c5984d36c14a19e4603ac6
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-13 20:19:25 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:19:25 up  8:34,  1 user,  load average: 0.24, 0.19, 0.38 e8a335f342ca6887b3cbb44ae5ad6b44f7ba4709

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -565,6 +565,7 @@ public class LLVMIRToRiscv {
                                         asm.instr("sw", allocator.allocate(x.variable_name), value_stack_addr.get(x.variable_name));
                                     } else {
                                         String reg = freshReg();
+                                        System.out.println("czxzczx");
                                         asm.instr("lw", reg, String.format("%d(sp)", Integer.parseInt(value_stack_addr.get(x.variable_name))));
                                         asm.instr("sw", allocator.allocate(x.variable_name), "0(" + reg + ")");
                                     }
