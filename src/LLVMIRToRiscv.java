1d7429e575eafe1e08f8fa481aed10258855bf0a
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-13 13:29:47 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:29:47 up  1:27,  1 user,  load average: 2.31, 1.53, 0.76 14688daef38440ded115362e5a345c2b2d55662b

--- src/LLVMIRToRiscv.java
+++ src/LLVMIRToRiscv.java
@@ -330,16 +330,8 @@ public class LLVMIRToRiscv {
                                     const_param_reg.put("x1"+i,((int)LLVM.LLVMConstIntGetSExtValue(constInt)));
                                 } else {
                                     if (allocator.allocate(name).contains("x")){
-                                        if(allocator.allocate(name).equals("x13")) System.out.println("crzx");
                                         int src_reg_num=Integer.parseInt(allocator.allocate(name).substring(allocator.allocate(name).indexOf("x")+1));
-                                        if(src_reg_num>=10&&src_reg_num<10 + Math.min(8, paramCount)){
-                                            if(LLVM.LLVMGetBasicBlockName(bb).getString().equals("if.then")&&LLVM.LLVMGetValueName(func).getString().equals("b")){
-                                                System.out.println(src_reg_num);
-                                                System.out.println(10+i);
-                                            }
-                                            calling_pass_param.put(src_reg_num,10+i);
-                                        }
-                                        else  asm.mv("x1" + i, allocator.allocate(name));
+                                        calling_pass_param.put(src_reg_num,10+i);
                                     }
                                     else {
                                         //String reg = evaluate(arg);
