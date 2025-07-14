8f3e3e7dfb1268988b41741c9d4d633187f0a2e3
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-14 15:49:14 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:49:14 up  7:09,  1 user,  load average: 2.42, 0.93, 0.38 5edeb2715a48fcced2f9473558f581d628461b41

--- tests/develop_test/gloabl_array_test.c
+++ tests/develop_test/gloabl_array_test.c
@@ -2,8 +2,8 @@ int ROWS_A=2;
 int COLS_A=3;
 int ROWS_B=3;
 int COLS_B=2;
-int result[2][2]={{1,1},{1,1}};
-void matrix_multiply(int a[2][3], int b[3][2]) {
+int result2[2][2]={{1,1},{1,1}};
+void matrix_multiply(int a[2][3], int b[3][2],int result[2][2]) {
     for (int i = 0; i < ROWS_A; i=i+1) {
         for (int j = 0; j < COLS_B; j=j+1) {
             result[i][j] = 0;
@@ -30,7 +30,7 @@ int main() {
     int result[2][2];
 
     // 计算矩阵乘法
-    matrix_multiply(matrix_a, matrix_b);
+    matrix_multiply(matrix_a, matrix_b,result);
 
     return result[1][1];
 }
