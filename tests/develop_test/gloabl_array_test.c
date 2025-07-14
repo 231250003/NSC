6d9f3cb4bba0b960c62e9a32aea3b62be2a43698
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-14 19:34:58 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:34:58 up 11:04,  1 user,  load average: 0.41, 0.31, 0.28 9577f0d4a5a86a58cd0eabeb4cc312de5368a9ce

--- tests/develop_test/gloabl_array_test.c
+++ tests/develop_test/gloabl_array_test.c
@@ -1,8 +1,8 @@
 int ROWS_A=2;
 int COLS_A=3;
 int ROWS_B=3;
-int COLS_B=COLS_A;
-//int resultsaasdas[5][2][3]={{1,ROWS_B}};
+int COLS_B=3;
+int resultsaasdas[5][2][3]={{1,ROWS_B}};
 void matrix_multiply(int a[2][3], int b[3][2],int result[2][2]) {
     for (int i = 0; i < ROWS_A; i=i+1) {
         for (int j = 0; j < COLS_B; j=j+1) {
