c178d8519da4360681875e9ebbc0fbf27bc2f4c0
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-15 21:00:39 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:00:39 up  3:30,  1 user,  load average: 0.21, 0.29, 0.36 6441740528a19227b70ca608584071899848b4ae

--- tests/develop_test/IRoptimizetest.c
+++ tests/develop_test/IRoptimizetest.c
@@ -1,14 +1,9 @@
 int main(){
     int i=0;
-    int n=6,m=6;
+    int n=6;
     while(i<n)  // i < n而不是i <= n
     {
-        int v=1;  // 每次外层循环都要重置v
-        while(v<=m)
-        {
 
-            v=v+1;
-        }
         i=i+1;
     }
     return 2;
