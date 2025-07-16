f0cbb0c2bc9918896907aea5e686492492a911af
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-16 15:02:07 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:02:07 up  6:24,  1 user,  load average: 1.36, 1.18, 0.72 8b26648872599dc265ef400391aa65db55f5c233

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -68,6 +68,7 @@ public class Main {
         for (LLVMValueRef func = LLVM.LLVMGetFirstFunction(module); func != null && !func.isNull(); func = LLVM.LLVMGetNextFunction(func)) {
             boolean flag1 = true, flag2=true,flag3 = false, flag4 = false;
             while (flag1 || flag2||flag3 || flag4) {
+                System.out.println("csd");
                 flag1 = optimization.constprop(func);
                 //LLVMDumpModule(module);
                 flag2=optimization.pointer_must_optimize(func);
