204bb0df76ca7a5758f8c6c84ed378f7403dbe7b
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-16 14:05:51 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:05:51 up  5:23,  1 user,  load average: 0.20, 0.72, 0.69 ef08e694a1ff1195e13c36a9c22b11cf427a5a1f

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -66,10 +66,10 @@ public class Main {
             while (remove_blocks_without_predecessors(func)) ;
         }
         for (LLVMValueRef func = LLVM.LLVMGetFirstFunction(module); func != null && !func.isNull(); func = LLVM.LLVMGetNextFunction(func)) {
-            boolean flag1 = true, flag2=false,flag3 = false, flag4 = false;
+            boolean flag1 = true, flag2=true,flag3 = false, flag4 = false;
             while (flag1 || flag2||flag3 || flag4) {
                 flag1 = optimization.constprop(func);
-                //flag2=optimization.pointer_must_optimize(func);
+                flag2=optimization.pointer_must_optimize(func);
                // flag4 = optimization.elem_unused(func);
                 //flag4 = optimization.elem_dead_code(func);
             }
