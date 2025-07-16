bfd84b8311a472386952835f8d08345bbc4660d1
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-16 19:54:36 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:54:36 up 11:43,  1 user,  load average: 0.40, 0.30, 0.29 ca74ad45330bb1418ddd6b0823a27558d8a6bf61

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -69,6 +69,7 @@ public class Main {
             boolean flag1 = true, flag2=true,flag3 = false;
             while (flag1 || flag2||flag3 ) {
                 flag1 = optimization.constprop(func);
+                LLVMDumpModule(module);
                 flag2 = optimization.elem_unused(func);
                 flag3 = optimization.elem_dead_code(func);
             }
