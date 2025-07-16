37142700b5110b432f943e12688fb5d9dbd0a2fe
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-16 15:13:18 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:13:18 up  6:36,  1 user,  load average: 0.32, 0.84, 0.79 181df5c0155e3a847196ad39a44147384bf19b0b

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -66,13 +66,13 @@ public class Main {
         }
        // LLVMDumpModule(module);
         for (LLVMValueRef func = LLVM.LLVMGetFirstFunction(module); func != null && !func.isNull(); func = LLVM.LLVMGetNextFunction(func)) {
-            boolean flag1 = true, flag2=true,flag3 = false, flag4 = false;
-            while (flag1 || flag2||flag3 || flag4) {
+            optimization.pointer_must_optimize(func);
+            boolean flag1 = true, flag2=false,flag3 = false;
+            while (flag1 || flag2||flag3 ) {
                 LLVMDumpModule(module);
                 flag1 = optimization.constprop(func);
-                flag2=optimization.pointer_must_optimize(func);
-               // flag3 = optimization.elem_unused(func);
-                //flag4 = optimization.elem_dead_code(func);
+               // flag2 = optimization.elem_unused(func);
+                //flag3 = optimization.elem_dead_code(func);
             }
         }
         BytePointer error = new BytePointer((Pointer) null);
