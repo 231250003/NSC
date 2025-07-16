1529f15eeaac9be20cd64ab5c41d04f0d598aceb
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-16 16:59:22 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:59:22 up  8:32,  1 user,  load average: 5.60, 3.73, 2.35 45d60a9c89a42b3b5e0a20412f10eae31a3a80df

--- src/main/java/com/example/LLVMIROptimization.java
+++ src/main/java/com/example/LLVMIROptimization.java
@@ -770,7 +770,7 @@ public class LLVMIROptimization {
             }
         }
         for (LLVMValueRef instr : toErase) {
-            System.out.println(LLVM.LLVMPrintValueToString(instr));
+            System.out.println(LLVM.LLVMPrintValueToString(instr).getString());
             LLVM.LLVMInstructionEraseFromParent(instr);
         }
         return ret;
