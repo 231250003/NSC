50b9ef05afe121faa15f585a68d43c3255424183
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-16 15:00:40 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:00:40 up  6:22,  1 user,  load average: 2.33, 1.08, 0.64 c149d05ef250c8b6af7c303656e89ccad2638e42

--- src/main/java/com/example/LLVMIROptimization.java
+++ src/main/java/com/example/LLVMIROptimization.java
@@ -649,7 +649,7 @@ public class LLVMIROptimization {
                     array_variable av = new array_variable(variable_name, array_name, array_dim, cur_offset, array_size);
                     boolean has_must_alias=false;
                     for(array_variable x:arrayVariable_ref){
-                        if(av.variable_name.equals("elemPtr27")&&x.variable_name.equals("elemPtr6")) System.out.println("safd");
+                        if(av.variable_name.equals("elemPtr27")) System.out.println("safd");
                         if(GraphColoringRegisterAllocator.naive_alias_must_analysis(av,x)==true){
                             has_must_alias=true;
                             alias_must_pointer.put(inst,elementptr_ref.get(x.variable_name));
