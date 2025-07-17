3f0ad3d4e8702ed3950e293fcf076ef7140ae06f
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-07-17 13:37:54 +0800

    > make 231250003 crz Linux DESKTOP-LJJBL1C 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:37:54 up  1:33,  1 user,  load average: 0.53, 0.71, 0.98 7f0c7c532aae55e625b2066b240eb8a77351468c

--- src/main/java/com/example/LLVMIROptimization.java
+++ src/main/java/com/example/LLVMIROptimization.java
@@ -942,7 +942,7 @@ public class LLVMIROptimization {
         }
         cleanUnreachableBlocks(func);
         buildgraph(func);
-        //while (simplifySingleInstructionBlocks(func));
+        while (simplifySingleInstructionBlocks(func));
         //while (remove_redundant_block(func)) ;
         return ret;
     }
