a60c5c2b5fc0dc14a365db7d4a3dc7a43b4fe76d
231250003-Chen rui ze <crz@DESKTOP-LJJBL1C.localdomain>
2025-07-17 16:40:05 +0800

    testing version1

--- tests.sh
+++ tests.sh
@@ -52,7 +52,7 @@ for sysy_file in "${FILE_LIST[@]}"; do
 
     # Step 4: 用 make run 生成 .ll 文件
     #mvn -q compile exec:java -Dexec.args="$sysy_file $ll_file"
-    make -s run SRCFILE="$sysy_file" OUTFILE="$ll_file"
+   mvn -q compile exec:java -Dexec.args="$sysy_file $ll_file"
     if [ $? -ne 0 ]; then
         echo "IR generation failed for $exe_file"
         continue
